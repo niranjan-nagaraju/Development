f7aeaa80eb12aa3edf7c00faa936feca69f6afac
Niranjan Nagaraju <vinithepooh@gmail.com>
2010-12-15 00:36:03 +0530

    Monoalphabetic cipher implementation completed (Haskell)

--- Cryptography/haskell/monoalphabetic_cipher.hs
+++ Cryptography/haskell/monoalphabetic_cipher.hs
@@ -1,24 +1,76 @@
+module MonoalphabeticCipher
+	where
+
 import IO
 import Char
 
-monoalpha_encrypt :: String -> String -> String
-monoalpha_encrypt plaintext keytext = 
+{- Encryption routine -}
+monoalphabetic_encrypt :: String -> String -> String
+monoalphabetic_encrypt plaintext enc_key_map = 
 	 map (\x -> 
 			if isAlpha(x)
-				then (keytext !! (ord(toUpper(x)) - ord('A')))
+				then (enc_key_map !! (ord(toLower(x)) - ord('a')))
 				else x
-				) plaintext
+		 ) plaintext
 
-{-
+
+{- Decryption routine -}
+monoalphabetic_decrypt :: String -> String -> String
+monoalphabetic_decrypt ciphertext dec_key_map = 
+	 map (\x -> 
+			if isAlpha(x)
+				then (dec_key_map !! (ord(toUpper(x)) - ord('A')))
+				else x
+		 ) ciphertext
+
+
+{- Create a lookup table for encryption; Pre-store as uppercase -}
+generate_enc_key_map :: String -> String
+generate_enc_key_map keymap = 
+	map toUpper keymap
+
+
+{- Change a character in "Str" to "c" at the specified position "pos" or /* str[i] = c */ -}
+change_char_at_pos :: Char -> Int -> String -> String
+change_char_at_pos c pos str = (take (pos) str) ++ [c] ++ (drop (pos+1) str)
+
+
+{- 
+ - Helper to Create a reverse lookup table 
+ - Recursively changes the reverse lookup keystring one character at a time.
+ -}
+generate_dec_key_map' :: Int -> String -> String -> String
+generate_dec_key_map' index keymap dec_key_map = 
+	if (index >= 0)
+		then generate_dec_key_map' (index-1) keymap (change_char_at_pos dec_c rev_index dec_key_map)
+		else dec_key_map
+	where 
+		c = (keymap !! index)
+		rev_index = ord(toLower(c)) - ord('a')
+		dec_c = chr(ord('a') + index)
+
+
+{- 
+ - Create a reverse lookup table for decryption 
+ - Initial dec_key_map is a list of 26 a's which are then replaced R-L with reverse lookup letters for decryption
+ -}
+generate_dec_key_map :: String -> String
+generate_dec_key_map keymap = generate_dec_key_map' 25 keymap (replicate 26 'a')
+
+
 main = do
-	let plaintext = "Meet me after the toga party"
+	let plaintext = "Hello World"
+	let keymap = "ZEBRASCDFGHIJKLMNOPQTUVWXY"
 	
-	keytext <- getLine
+	let enc_key_map = generate_enc_key_map keymap
+	let dec_key_map = generate_dec_key_map keymap
 	
-	if length(keytext) != 26
-		then 
+	putStrLn ("Enc Map: " ++ enc_key_map)
+	putStrLn ("Dec Map: " ++ dec_key_map)
 
-	let ciphertext = (monoalpha_encrypt plaintext 3)
-	putStrLn ("Plaintext: " ++ plaintext)
-	putStrLn ("Ciphertext: " ++ ciphertext)
-	-}
+	let ciphertext = monoalphabetic_encrypt plaintext enc_key_map
+	let dec_plaintext = monoalphabetic_decrypt ciphertext dec_key_map
+
+	putStrLn ("Encrypted Ciphertext: " ++ ciphertext)
+	putStrLn ("Decrypted Plaintext: " ++ dec_plaintext)
+ 
