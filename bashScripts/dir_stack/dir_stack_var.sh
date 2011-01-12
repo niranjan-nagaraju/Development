7edbbe9eedd1c6c49f147ef59cd18c6cb6d262d1
Niranjan Nagaraju <vinithepooh@gmail.com>
2011-01-12 23:17:50 +0530

    var version almost done; dpop after a series of dpush wont take u 1 dir back. D'oh

--- bashScripts/dir_stack/dir_stack_var.sh
+++ bashScripts/dir_stack/dir_stack_var.sh
@@ -1,5 +1,4 @@
-
-# push te directory onto the top of the stack; 
+# push the directory onto the top of the stack; 
 # Only first parameter is considered.
 # More parameters if present will be ignored.
 function dpush()
@@ -32,19 +31,89 @@ function dpush()
 	return 0
 }
 
+# Pop a directory entry of the top of the stack
+# or remove an entry or one at the specified index
 function dpop()
 {
-	if [ $# != 3 -o $# -o 1 ] 
+	# 1, 2 or 0 parameters are valid
+	# No pop operation can be done on an empty stack
+	if [ ${#DIRECTORY_STACK[*]} == 0 ]
 	then
-		echo "dpop: Usage: 
+		echo "dpop: Stack empty"
+		return 1
+	fi
+
+	case $# in
+		0) # Remove directory entry off the top of the stack and cd to it
+			stk_idx=`expr ${#DIRECTORY_STACK[*]} - 1`
+			dir_entry=${DIRECTORY_STACK[$stk_idx]}
+			unset DIRECTORY_STACK[$stk_idx]
+
+			cd $dir_entry
+			return 0
+		;;
+
+		1) # Remove specified directory if found
+			dir_entry=$1
+			i=0
+			for dir_in_stk in ${DIRECTORY_STACK[*]}
+			do
+				if [ $dir_in_stk == $dir_entry ]
+				then
+					unset DIRECTORY_STACK[$stk_idx]
+					cd $dir_entry
+
+					return 0
+				fi
+				(( i = $i + 1 ))
+			done
+			return 1
+		;;
+
+		2) # 2 arguments, an index is expected
+			if [ $1 != '-i' ]
+			then
+				echo "dpop: Incorrect switch, -i expected"
+				return 1
+			fi
+	
+			shift;
+			stk_idx=$1
+
+			# Invalid index
+			if [ $stk_idx < 0 -o $stk_idx >= ${#DIRECTORY_STACK[*]} ]
+			then
+				echo "dpop: Invalid Index"
+				return 1
+			fi
+	
+			unset ${DIRECTORY_STACK[$stk_idx]}
+			return 0
+		;;
+
+		*) # Invalid number of arguments
+			echo "dpop: Usage: dpop [-i index] [directory]" 
+			return 1
+		;;
+
+	esac
 }
 
 function didx()
 {
-	echo "blah";
+	if [ ${#DIRECTORY_STACK[*]} == 0 ]
+	then
+		echo "didx: Stack empty"
+		return 1
+	fi
+
+	for i in ${DIRECTORY_STACK[*]}
+	do
+		echo $i
+	done
 }
 
 function dclr()
 {
-	echo "blah";
+	unset DIRECTORY_STACK
 }
