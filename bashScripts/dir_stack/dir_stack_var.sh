f5cfe4e71816d1a7b07da7404af3c32a71853dd5
Niranjan Nagaraju <vinithepooh@gmail.com>
2011-01-20 22:52:46 +0530

    Use DIRSTACK and cd - changes

--- bashScripts/dir_stack/dir_stack_var.sh
+++ bashScripts/dir_stack/dir_stack_var.sh
@@ -3,11 +3,21 @@
 # More parameters if present will be ignored.
 function dpush()
 {
-	# Swap {tos} and {tos-1}
+	# On no arguments, behave like cd -; Swap {tos} and {tos-1}
 	if [ $# == 0 ]
 	then
-		DIRECTORY_STACK=( ${DIRECTORY_STACK[1]} ${DIRECTORY_STACK[0]} ${DIRECTORY_STACK[*]:2} ) 
-		cd $DIRECTORY_STACK
+		# There should be atleast 2 entries, One last working directory and one current directory
+		if [ ${#DIR_STACK[*]} -lt 2 ]
+		then
+			echo "dpush: Stack empty"
+			return 1;
+		fi
+
+		DIR_STACK=( ${DIR_STACK[1]} ${DIR_STACK[0]} ${DIR_STACK[*]:2} ) 
+
+		cd $DIR_STACK
+
+		return 0
 	fi
 
 	# Directory does not exist
@@ -24,15 +34,10 @@ function dpush()
 		return 1;
 	fi
 
-	# Stack is empty
-	if [ -z ${DIRECTORY_STACK} ]
-	then
-		DIRECTORY_STACK[0]=`readlink -f $1` # push absolute path
-	else	# New TOS is length(stack) i.e. max_index+1
-		DIRECTORY_STACK[${#DIRECTORY_STACK[*]}]=`readlink -f $1`
-	fi
+	# Push new diretctory entry onto the top of the stack.
+	DIR_STACK=( $1 ${DIR_STACK[*]} )
 
-	echo ${DIRECTORY_STACK[*]}
+	echo ${DIR_STACK[*]}
 	cd $1
 
 	return 0
@@ -44,7 +49,7 @@ function dpop()
 {
 	# 1, 2 or 0 parameters are valid
 	# No pop operation can be done on an empty stack
-	if [ ${#DIRECTORY_STACK[*]} == 0 ]
+	if [ ${#DIR_STACK[*]} == 0 ]
 	then
 		echo "dpop: Stack empty"
 		return 1
@@ -52,9 +57,9 @@ function dpop()
 
 	case $# in
 		0) # Remove directory entry off the top of the stack and cd to it
-			stk_idx=`expr ${#DIRECTORY_STACK[*]} - 2`
-			dir_entry=${DIRECTORY_STACK[$stk_idx]}
-			unset DIRECTORY_STACK[$stk_idx]
+			stk_idx=`expr ${#DIR_STACK[*]} - 2`
+			dir_entry=${DIR_STACK[$stk_idx]}
+			unset DIR_STACK[$stk_idx]
 
 			cd $dir_entry
 			return 0
@@ -63,11 +68,11 @@ function dpop()
 		1) # Remove specified directory if found
 			dir_entry=`readlink -f $1`
 			i=0
-			for dir_in_stk in ${DIRECTORY_STACK[*]}
+			for dir_in_stk in ${DIR_STACK[*]}
 			do
 				if [ $dir_in_stk == $dir_entry ]
 				then
-					unset DIRECTORY_STACK[$stk_idx]
+					unset DIR_STACK[$stk_idx]
 					cd $dir_entry
 
 					return 0
@@ -88,14 +93,14 @@ function dpop()
 			stk_idx=$1
 
 			# Invalid index
-			if [ $stk_idx < 0 -o $stk_idx >= ${#DIRECTORY_STACK[*]} ]
+			if [ $stk_idx < 0 -o $stk_idx >= ${#DIR_STACK[*]} ]
 			then
 				echo "dpop: Invalid Index"
 				return 1
 			fi
 	
 			i=0
-			for dir_in_stk in ${DIRECTORY_STACK[*]}
+			for dir_in_stk in ${DIR_STACK[*]}
 			do
 				if [ $stk_idx == $i ]
 				then
@@ -105,7 +110,7 @@ function dpop()
 			done
 
 			cd $dir_in_stk
-			unset ${DIRECTORY_STACK[$i]}
+			unset ${DIR_STACK[$i]}
 			return 0
 		;;
 
@@ -119,13 +124,13 @@ function dpop()
 
 function didx()
 {
-	if [ ${#DIRECTORY_STACK[*]} == 0 ]
+	if [ ${#DIR_STACK[*]} == 0 ]
 	then
 		echo "didx: Stack empty"
 		return 1
 	fi
 
-	for i in ${DIRECTORY_STACK[*]}
+	for i in ${DIR_STACK[*]}
 	do
 		echo $i
 	done
@@ -133,5 +138,5 @@ function didx()
 
 function dclr()
 {
-	unset DIRECTORY_STACK
+	unset DIR_STACK
 }
