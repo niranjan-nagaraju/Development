dd6860bcec94c34bd8c4a574037e3843dd8b585e
Niranjan Nagaraju <vinithepooh@gmail.com>
2011-01-24 22:23:37 +0530

    Inital matrix stub (to be used for implementing hill cipher now and later extended )

---
+++ python/Data_Structures/matrix/matrix.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+class Matrix:
+	def __init__(self, rows, cols):
+		self.rows = rows
+		self.cols = cols
+		self.matrix = [][]
+
+	def __init__(self, rows, cols, matrix_list):
+
+
+	
