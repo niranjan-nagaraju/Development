5bae8d6425f4219000eff3ec1445382d129ac492
Niranjan Nagaraju <vinithepooh@gmail.com>
2011-01-24 22:11:54 +0530

    Sanitized Playfair key and prepare key matrix for lookup

--- Cryptography/python/substitution_ciphers/playfair_cipher/playfair_cipher.py
+++ Cryptography/python/substitution_ciphers/playfair_cipher/playfair_cipher.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python
 
-original_key = "monarchyyy"
-alphabets = range(ord('a'), ord('z')+1)
+class PlayfairCipher:
+	def __init__(self, original_key):
+		self.original_key = original_key
+		self.key_matrix = self.prepare_key_matrix(original_key) # Create a 5x5 lookup matrix
 
-# Eliminate duplicate characters in key
-key = [] 
-[key.append(k) for k in original_key if k not in key]
-print key
+	# Create a 5x5 key matrix
+	@staticmethod
+	def prepare_key_matrix(original_key):
+		alphabets = range(ord('a'), ord('z')+1)
 
-key_matrix = [k.upper() for k in key ] + [chr(k).upper() for k in alphabets if (chr(k) not in key) and (chr(k).upper() != 'I') ]
+		# Eliminate duplicate characters in key
+		key = [] 
+		[key.append(k) for k in original_key if k not in key]	# Eliminate duplicate characters in key
 
-print key_matrix
+		key_matrix = [k.upper() for k in key ] + [chr(k).upper() for k in alphabets if (chr(k) not in key) and (chr(k).upper() != 'I') ]
+
+		return key_matrix
+
+def main():
+	p = PlayfairCipher("mmonaarchyy")
+
+	print 'Key Matrix: ', p.key_matrix
+
+
+if __name__ == "__main__":
+	main()
