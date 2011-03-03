4a1044feb10ec938ff461d967287f2bcfc51d12b
Niranjan Nagaraju <vinithepooh@gmail.com>
2011-03-03 17:35:47 +0530

    99 Haskell Problems - 20. removeAt

--- haskell/99_haskell_problems/20_removeAt.hs
+++ haskell/99_haskell_problems/20_removeAt.hs
@@ -1,4 +1,10 @@
 {- Remove the K'th element from a list. -}
 
 removeAt k list  = 
-	take (k) list ++ drop k list
+	take (k-1) list ++ drop k list
+
+main = do
+	print $ removeAt 1 "abcd"
+	print $ removeAt 2 "abcd"	
+	print $ removeAt 3 "abcd"	
+	print $ removeAt 4 "abcd"	
