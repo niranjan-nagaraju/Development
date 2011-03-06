a639f7c22aeeeb23daf88b3f40efe4500c44175c
Niranjan Nagaraju <vinithepooh@gmail.com>
2011-03-06 20:27:38 +0530

    99 Haskell problems - 1. myLast use null list check

--- haskell/99_haskell_problems/1_myLast.hs
+++ haskell/99_haskell_problems/1_myLast.hs
@@ -1,11 +1,10 @@
 {- Find the last element of a list. -}
 
-myLast (x:xs) = 
-	if (xs == [])
+myLast (x:xs) =
+	if (null xs)
 		then x
 		else myLast xs
 
-
 main = do
 	print $ myLast [1,2,3,4]
-	print $ myLast ['x', 'y', 'z']
+	print (myLast ['x', 'y', 'z'])
