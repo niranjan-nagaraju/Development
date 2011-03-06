0ce3e6eba32bbd7085536263f259228f71c4f27d
Niranjan Nagaraju <vinithepooh@gmail.com>
2011-03-06 22:50:33 +0530

    Started encryption logic for S_DES

--- Cryptography/python/block_ciphers/S_DES/S_DES.py
+++ Cryptography/python/block_ciphers/S_DES/S_DES.py
@@ -2,6 +2,11 @@
 P10 = [3, 5, 2, 7, 4, 10, 1, 9, 8, 6]
 P8 = [6, 3, 7, 4, 8, 5, 10, 9]
 
+IP = [2, 6, 3, 1, 4, 8, 5, 7]
+IP_inv = [4, 1, 3, 5, 7, 2, 8, 6]
+
+E_P = [4, 1, 2, 3, 2, 3, 4, 1]
+
 def apply_p10(key):
 	permuted_key = []
 	for i in range(0, 10):
@@ -41,5 +46,50 @@ def generate_key(key):
 	k2 = apply_p8(ls2_l + ls2_r)
 	print 'K2 ->', k2
 
+
+def apply_ip (plaintext):
+	permuted_plaintext = []
+	for i in range(0, 8):
+		permuted_plaintext.append(plaintext[IP[i]-1])
+
+	return permuted_plaintext
+
+
+def apply_ip_inv (text):
+	permuted_text = []
+	for i in range(0, 8):
+		permuted_text.append(text[IP_inv[i]-1])
+
+	return permuted_text
+
+def xor (n1, b1):
+	if ( n1 == b1 ):
+		return 0
+	else:
+		return 1
+
+def xor_4 (n4, b4):
+	xored = []
+	for i in [0, 1, 2, 3]:
+		xored.append(xor(n4[i], b4[i]))
+	
+	return xored
+
+
+def fk (text, key):
+	l = f[:4]
+	r = f[4:]
+
+	return ((xor_4 (l, e_p(r, key))) + r)
+	
+
+def e_p (n4, key):
+	x8 = []
+	for i in range(0, 8):
+		x8.append(n4[e_p[i]-1])
+	
+
+
+	
 
 generate_key([1,0,1,0,0,0,0,0,1,0])
