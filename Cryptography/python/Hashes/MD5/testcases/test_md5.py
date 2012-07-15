1d9e8810d22f23105b9918bd56f00f89b3754e21
Niranjan Nagaraju <vinithepooh@gmail.com>
2012-07-15 17:23:21 +0530

    No Hash testcase is complete without the 'The quick brown fox jumps over the lazy dog' input ;)

--- Cryptography/python/Hashes/MD5/testcases/test_md5.py
+++ Cryptography/python/Hashes/MD5/testcases/test_md5.py
@@ -7,7 +7,8 @@ def run_test (inText):
 def runTestCases ():
 	test_inputs = [ "", "a", "abc", "message digest", "abcdefghijklmnopqrstuvwxyz", \
 					"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789", \
-					"12345678901234567890123456789012345678901234567890123456789012345678901234567890" \
+					"12345678901234567890123456789012345678901234567890123456789012345678901234567890", \
+					"The quick brown fox jumps over the lazy dog" \
 					]
 
 	for i in test_inputs:
@@ -26,5 +27,6 @@ MD5 ("abcdefghijklmnopqrstuvwxyz") = c3fcd3d76192e4007dfb496cca67e13b
 MD5 ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789") =
 d174ab98d277d9f5a5611c2c9f419d9f
 MD5 ("12345678901234567890123456789012345678901234567890123456789012345678901234567890") = 57edf4a22be3c955ac49da2e2107b67a
+MD5 ("The quick brown fox jumps over the lazy dog"): 9e107d9d372bb6826bd81d3542a419d6
 '''
 
