0e97f6de5a2ed70ec0257237a02a0bd5d2a754cc
Niranjan Nagaraju <vinithepooh@gmail.com>
2012-08-15 19:23:36 +0530

    Int to string: handle negative numbers

--- python/Algorithms/intToStr.py
+++ python/Algorithms/intToStr.py
@@ -1,10 +1,29 @@
-def intToStr (x, strList):
+#!/usr/bin/python
+
+# Recursive helper to convert a number to string
+def intToStr_h (x, strList):
 	if (x == 0):
 		return
 
-	intToStr(x/10, strList)
+	intToStr_h (x/10, strList)
 	strList += [chr((x%10) + ord('0'))]
 
-string = []
-intToStr(1234, string)
-print "".join(string)
+
+# Handle +ve and -ve numbers
+def intToStr (x):
+	string = []
+
+	# if the number is -ve, make it +ve, but store the sign in the string
+	if x < 0:
+		x = -x
+		string += '-'
+
+	intToStr_h (x, string)
+	return "".join(string)
+
+
+def main():
+	print intToStr(1234), intToStr(-12)
+
+if __name__ == "__main__":
+	main()
