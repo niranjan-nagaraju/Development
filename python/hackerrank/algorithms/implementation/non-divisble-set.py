c1ccd557aa001de678a408f52cb898d3488a9662
Niranjan Nagaraju <vinithepooh@gmail.com>
2016-08-18 22:30:28 +0530

    Non divisible set -- solved!

--- python/hackerrank/algorithms/implementation/non-divisble-set.py
+++ python/hackerrank/algorithms/implementation/non-divisble-set.py
@@ -30,23 +30,55 @@ Explanation
 '''
 
 
-# compare pairs (nC2) and return subset whose pairs aren't divisible by k
-# e.g. 1 7 2 4
-#	1,7 1,2, 1,4
-#	7,2 7,4
-#	2,4
-def compare_pairs_and_get_subset(l, n , k):
-	non_divisible_subset = set()
-	for i in xrange(n-1):
-		for j in xrange(i+1, n):
-			if (l[i] + l[j]) % k != 0:
-				non_divisible_subset.add(l[i])
-				non_divisible_subset.add(l[j])
-				print 'Adding ', (l[i], l[j])
 
-	return len(non_divisible_subset)
+
+def get_max_non_divisible_subset(l, n, k):
+	'''
+	(a+b) % k == a%k + b%k
+	k | (a+b) if (a%k) + (b%k) == k OR 0 (0, when k|a and k|b)
+	'''
+	# 'k' buckets, each will be filled based on how many numbers exist with that modulo
+	# mods[i] will be counted for every ai % k == i
+	mods = [0]*k
+
+	for x in l:
+		mods[x % k] += 1
+	
+
+	'''
+	Elements from mods[i] and mods[k-i] cant both be there in the same subset as their sum will be divisible by k
+	Pick max(mods[i], mods[k-i]) to be in the subset 
+
+	mods[0] are all divisible, ONLY one of its elements can be included in the non-divisible-subset
+	'''
+
+	nd_subset_count = 1 if mods[0] > 0 else 0 # or just min(mods[0], 1)
+
+	i = 1 
+	j = k-i
+
+	while i < j:
+		nd_subset_count += max(mods[i], mods[j])
+		i += 1
+		j -= 1
+
+	'''
+	if k is even, then it'll have odd number of remainders
+	e.g. 
+	6: (1+5) (2+4) (3+3) => Only one can be included from mods[3]
+		The one remainder that repeats, ==> k/2 (because i == k-i => i = k/2)
+	In contrast Odd k=5: (1+4), (2+3)
+	'''
+
+	# if k is even, include one element from mods[k/2] iff atleast one
+	# element exists in the input list, with % == k/2
+	if (k & 1 == 0):
+		nd_subset_count += (1 if mods[k/2] > 0 else 0)
+
+	return nd_subset_count
 
 
 n, k = map(int, raw_input().split())
 l = map(int, raw_input().split())
-print compare_pairs_and_get_subset(l, n, k)
+print get_max_non_divisible_subset(l, n, k)
+
