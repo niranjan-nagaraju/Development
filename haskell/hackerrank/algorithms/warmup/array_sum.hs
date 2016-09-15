63cee95abb873c1810ae0b96543a1d8d186a73de
Niranjan Nagaraju <vinithepooh@gmail.com>
2016-09-16 00:45:06 +0530

    Read input array and calculate sum

---
+++ haskell/hackerrank/algorithms/warmup/array_sum.hs
@@ -0,0 +1,33 @@
+{-
+https://www.hackerrank.com/challenges/simple-array-sum
+
+Given an array of N integers, can you find the sum of its elements?
+
+Input Format
+
+The first line contains an integer, N, denoting the size of the array.
+The second line contains N space-separated integers representing the array's elements.
+
+Output Format
+
+Print the sum of the array's elements as a single integer.
+
+Sample Input
+
+6
+1 2 3 4 10 11
+
+Sample Output
+
+31
+-}
+
+parseStringToIntArray :: String -> [Int]
+parseStringToIntArray arrayStr =
+	map read.words $ arrayStr
+
+
+main = do
+	n <- readLn :: IO Int
+	str <- getLine
+	print $ sum (parseStringToIntArray str)
