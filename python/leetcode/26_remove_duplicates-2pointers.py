a50ff70b6c332586fef97a03c96f79a0674189e0
Niranjan Nagaraju <vinithepooh@gmail.com>
2016-11-23 19:31:19 +0530

    Add TCs and print list at every iteration - This works for all repetitions, not just if the elements are strictly duplicates

--- python/leetcode/26_remove_duplicates-2pointers.py
+++ python/leetcode/26_remove_duplicates-2pointers.py
@@ -3,8 +3,7 @@ https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 
 Given a sorted array, remove the duplicates in place such that each element
 appear only once and return the new length.
-i . strictly duplicates, not arbitrarily repeating 'n'
-ii. It doesn't matter what you leave beyond the new length. 
+i. It doesn't matter what you leave beyond the new length. 
 
 [1,1,2] => 2
 '''
@@ -14,6 +13,10 @@ class Solution(object):
 		:type nums: List[int]
 		:rtype: int
 		"""
+
+		if nums == []:
+			return 0
+
 		i = 0
 		j = 1
 		while True:
@@ -22,6 +25,7 @@ class Solution(object):
 					i += 1
 					nums[i] = nums[j]
 
+				print nums
 				j = j + 1
 
 			except IndexError:
@@ -30,7 +34,44 @@ class Solution(object):
 		return i+1
 
 obj = Solution()
-l = [1,1,2,2,3]
+l = [1,1,2,3]
 print obj.removeDuplicates(l), l
 
-        
+
+''' 
+Testcases:
+TC: 1
+l = [1,1,2,3]
+[1, 1, 2, 3]
+[1, 2, 2, 3]
+[1, 2, 3, 3]
+3 [1, 2, 3, 3]
+
+TC: 2
+l = [1,1,1,2,2,2,3,4,4,4]
+[1, 1, 1, 2, 2, 2, 3, 4, 4, 4]
+[1, 1, 1, 2, 2, 2, 3, 4, 4, 4]
+[1, 2, 1, 2, 2, 2, 3, 4, 4, 4]
+[1, 2, 1, 2, 2, 2, 3, 4, 4, 4]
+[1, 2, 1, 2, 2, 2, 3, 4, 4, 4]
+[1, 2, 3, 2, 2, 2, 3, 4, 4, 4]
+[1, 2, 3, 4, 2, 2, 3, 4, 4, 4]
+[1, 2, 3, 4, 2, 2, 3, 4, 4, 4]
+[1, 2, 3, 4, 2, 2, 3, 4, 4, 4]
+4 [1, 2, 3, 4, 2, 2, 3, 4, 4, 4]
+
+
+TC: 3
+l = [1,1,1,1,2,2,3,4,4,4]
+[1, 1, 1, 1, 2, 2, 3, 4, 4, 4]
+[1, 1, 1, 1, 2, 2, 3, 4, 4, 4]
+[1, 1, 1, 1, 2, 2, 3, 4, 4, 4]
+[1, 2, 1, 1, 2, 2, 3, 4, 4, 4]
+[1, 2, 1, 1, 2, 2, 3, 4, 4, 4]
+[1, 2, 3, 1, 2, 2, 3, 4, 4, 4]
+[1, 2, 3, 4, 2, 2, 3, 4, 4, 4]
+[1, 2, 3, 4, 2, 2, 3, 4, 4, 4]
+[1, 2, 3, 4, 2, 2, 3, 4, 4, 4]
+4 [1, 2, 3, 4, 2, 2, 3, 4, 4, 4]
+
+'''
