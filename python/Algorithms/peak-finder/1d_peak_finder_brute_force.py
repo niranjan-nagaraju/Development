7aba6e6da89d900ed7f1ef3cfbeb0832d27b8e25
Niranjan Nagaraju <vinithepooh@gmail.com>
2016-12-13 19:51:15 +0530

    Update testcases - 1D peak finder

--- python/Algorithms/peak-finder/1d_peak_finder_brute_force.py
+++ python/Algorithms/peak-finder/1d_peak_finder_brute_force.py
@@ -21,20 +21,26 @@ Testcases:
 
 [14:18:45 peak-finder]$ python 1d_peak_finder_brute_force.py 
 1 2 3 4
-4
+Peak 4 is at index 3
+
 [14:18:59 peak-finder]$ python 1d_peak_finder_brute_force.py 
 4 3 2 1
-4
+Peak 4 is at index 0
+
 [14:19:03 peak-finder]$ python 1d_peak_finder_brute_force.py 
 2 1 3 4
-2
+Peak 2 is at index 0
+
 [14:19:14 peak-finder]$ python 1d_peak_finder_brute_force.py 
 1 3 2 4
-3
+Peak 3 is at index 1
+
 [14:19:43 peak-finder]$ python 1d_peak_finder_brute_force.py 
 1 3 4 2
-4
+Peak 4 is at index 2
+
 [14:20:03 peak-finder]$ python 1d_peak_finder_brute_force.py 
 1 1 2 3
-1
+Peak 1 is at index 0
+
 '''
