e7973dcb7500a71ea36d26404790ff92288d6127
Niranjan Nagaraju <vinithepooh@gmail.com>
2016-12-27 16:35:17 +0530

    Remove compare wrapper from linear search code; Moved to common library

--- c/Algorithms/search/src/linear_search.c
+++ c/Algorithms/search/src/linear_search.c
@@ -1,15 +1,6 @@
 #include <search.h>
 #include <common.h>
 
-/** TODO: Move to common */
-int 
-compareAtInt(void *list, int idx, void *key, comparefn compare)
-{
-	/** for now, assume int array */
-	int *l = list;
-
-	return compare(l[idx], key);
-}
 
 int
 linear_search(void *list, int n, void *key, comparefn compare)
@@ -21,7 +12,7 @@ linear_search(void *list, int n, void *key, comparefn compare)
 		compare = compareInts;
 
 	for(i=0; i<n; i++) {
-		if(compareAtInt(list, i, key, compare) == 0) {
+		if(compare(list, i, key, -1) == 0) {
 			return i;
 		}
 	}
