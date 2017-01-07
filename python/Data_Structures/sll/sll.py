f98a4d6dce1feee61179ccb3f108092ce7d9d471
Niranjan Nagaraju <vinithepooh@gmail.com>
2017-01-08 00:01:12 +0530

    SLL - Insert, append and print

--- python/Data_Structures/sll/sll.py
+++ python/Data_Structures/sll/sll.py
@@ -2,27 +2,55 @@
 class Node:
 	def __init__(self, value):
 		self.value = value
-		self.next = 0
+		self.next = None
+
+	def __str__(self):
+		return str(self.value)
+
 
 class SLL:
 	def __init__(self):
-		self.head = Node(0)
+		self.head = None
+		self.tail = None
+		self.size = 0
 
+	# Insert at front
 	def insert(self, value):
 		node = Node(value)
+		self.size += 1
+
 		node.next = self.head
 		self.head = node
 
-	def show(self):
+		if (not self.tail):
+			self.tail = node
+
+	# Append to the rear
+	def append(self, value):
+		node = Node(value)
+
+		self.size += 1
+
+		self.tail.next = node
+		self.tail = node
+
+	def __str__(self):
+		sll_str = '[' + str(self.size) + ']: '
 		trav = self.head
-		while (trav.next != 0):
-			print trav.value,
+		while (trav):
+			sll_str += str(trav) + ' '
 			trav = trav.next
+
+		return sll_str
+
 
 sll = SLL()
 sll.insert(1)
 sll.insert(3)
 sll.insert(4)
 sll.insert(2)
+sll.append(1)
+sll.append(2)
+sll.append(3)
 
-sll.show()
+print sll
