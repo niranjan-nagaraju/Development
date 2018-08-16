424242b0df98301c8fa376c3139967e292c1a4e2
Niranjan Nagaraju <vinithepooh@gmail.com>
2018-08-16 20:47:02 +0530

    Add repr and exceptions for underflow and overflow conditions

--- python/data_structures/array/stack/stack.py
+++ python/data_structures/array/stack/stack.py
@@ -1,13 +1,24 @@
+# Exceptions for the stack class
+class OverFlowError(Exception):
+	''' Stack Overflow '''
+	pass
+
+class UnderFlowError(Exception):
+	''' Stack Underflow '''
+	pass
+
+
 class Stack(object):
 	def __init__(self, capacity=0):
 		self.capacity = capacity # 0 capacity => no checks for limits
 		self.size = 0
 		self.tos = -1
-		self.items = [0] * capacity # TODO: 0 capacity
+		self.items = [0] * capacity 
 
 	def push(self, item):
 		if self.capacity != 0 and self.capacity == self.size:
 			# overflow
+			raise OverFlowError
 			return
 
 		self.size += 1
@@ -17,7 +28,8 @@ class Stack(object):
 	def pop(self):
 		if self.size == 0:
 			# underflow
-			return
+			raise UnderFlowError
+			return None
 		
 		self.size -= 1
 		item = self.items[self.tos]
@@ -33,14 +45,36 @@ class Stack(object):
 		return stack_str
 
 
-if __name__ == '__main__':
-	stack = Stack(10)
-	stack.pop() # shouldn't crash
-	for i in range(1, 6):
-		print 'Pushing ', i
-		stack.push(i)
-		print stack
+	def __repr__(self):
+		return "[%d]: %r" %(self.size, self.items[:self.size])
+
+
+def TC():
+	stack = Stack(5)
+	try:
+		stack.pop() # shouldn't crash
+	except UnderFlowError:
+		print 'Stack Underflow - popped an empty stack!'
 
 	for i in range(1, 7):
-		print 'Popping ', stack.pop()
-		print stack
+		try:
+			print 'Pushing ', i,
+			stack.push(i)
+			print "Current stack: ", stack
+		except OverFlowError:
+			print 'Stack Overflow!, stack capacity:', stack.capacity
+
+	print 'repr: %r' %(stack)
+
+	for i in range(1, 7):
+		try:
+			print 'Popping ', stack.pop(),
+			print "Current stack: ", stack
+		except UnderFlowError:
+			print "Stack Underflow!"
+
+	assert(stack.tos  == -1)
+
+
+if __name__ == '__main__':
+	TC()
