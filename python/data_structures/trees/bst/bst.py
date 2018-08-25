01c7e33f34479a9587d970d2be2169f5e0b13ed9
Niranjan Nagaraju <vinithepooh@gmail.com>
2018-08-25 23:39:08 +0530

    Enable preorder traversal take a callback function to aggregate the traversed nodes as the caller sees fit; if no callback is specified, default to print

--- python/data_structures/trees/bst/bst.py
+++ python/data_structures/trees/bst/bst.py
@@ -6,14 +6,19 @@ class BST:
 	def __init__(self, root=None):
 		self.root = root
 
-	def preorder_traversal(self, root):
+	def preorder_traversal(self, root, aggregate_fn=None, **kwargs):
 		if not root:
 			return
 
-		print "%s" %(root),
-		self.preorder_traversal(root.left)
-		self.preorder_traversal(root.right)
+		# if no aggregate function callback is specified,
+		# just print the current node's contents
+		if aggregate_fn is None:
+			print "%s" %(root),
+		else:
+			aggregate_fn(kwargs, root)
 
+		self.preorder_traversal(root.left, aggregate_fn, **kwargs) 
+		self.preorder_traversal(root.right, aggregate_fn, **kwargs)
 
 
 if __name__ == "__main__":
@@ -30,3 +35,8 @@ if __name__ == "__main__":
 	bst = BST(root)
 	bst.preorder_traversal(root)
 
+	l = []
+	# define a lambda function that collates individual node values into a list
+	collate_fn = lambda kwargs, data : kwargs['lst'].append(data.value)
+	bst.preorder_traversal(root, collate_fn, lst=l)
+	assert (l == ['+', 'a', '*', 'b', 'c'])
