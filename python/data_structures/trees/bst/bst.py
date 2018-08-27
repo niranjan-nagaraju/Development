64113f0a30e2f9bc57d7323c970a58faa3a8a597
Niranjan Nagaraju <vinithepooh@gmail.com>
2018-08-27 21:32:58 +0530

    Add postorder and inorder traversal functions

--- python/data_structures/trees/bst/bst.py
+++ python/data_structures/trees/bst/bst.py
@@ -1,11 +1,14 @@
 # A Binary Search Tree
 
 from node import *
+import sys
 
 class BST:
-	def __init__(self, root=None):
+	def __init__(self, root=None, size=0):
 		self.root = root
+		self.size = size
 
+	# preorder traversal (R,l,r, l:left, r:right, R:Root)
 	def preorder_traversal(self, root, aggregate_fn=None, **kwargs):
 		if not root:
 			return
@@ -13,15 +16,45 @@ class BST:
 		# if no aggregate function callback is specified,
 		# just print the current node's contents
 		if aggregate_fn is None:
-			print "%s" %(root),
-		else:
-			aggregate_fn(kwargs, root)
+			aggregate_fn = lambda x,y : sys.stdout.write(str(y))
+
+		aggregate_fn(kwargs, root)
 
 		self.preorder_traversal(root.left, aggregate_fn, **kwargs) 
 		self.preorder_traversal(root.right, aggregate_fn, **kwargs)
 
 
-if __name__ == "__main__":
+	# post-order traversal (l,r,R, l:left, r:right, R:Root)
+	def postorder_traversal(self, root, aggregate_fn=None, **kwargs):
+		if not root:
+			return
+
+		# if no aggregate function callback is specified,
+		# just print the current node's contents
+		if aggregate_fn is None:
+			aggregate_fn = lambda x,y : sys.stdout.write(str(y))
+
+		self.postorder_traversal(root.left, aggregate_fn, **kwargs) 
+		self.postorder_traversal(root.right, aggregate_fn, **kwargs)
+		aggregate_fn(kwargs, root)
+
+
+	# In-order traversal (l,r,R, l:left, r:right, R:Root)
+	def inorder_traversal(self, root, aggregate_fn=None, **kwargs):
+		if not root:
+			return
+
+		# if no aggregate function callback is specified,
+		# just print the current node's contents
+		if aggregate_fn is None:
+			aggregate_fn = lambda x,y : sys.stdout.write(str(y))
+
+		self.inorder_traversal(root.left, aggregate_fn, **kwargs) 
+		aggregate_fn(kwargs, root)
+		self.inorder_traversal(root.right, aggregate_fn, **kwargs)
+
+
+def test_traversals():
 	# prefix equation tree : "+a*bc"
 	root = Node("+")
 	lnode = Node("a")
@@ -33,10 +66,33 @@ if __name__ == "__main__":
 	rnode.setChildren(rlnode, rrnode)
 
 	bst = BST(root)
+	print 'Preorder: ',
 	bst.preorder_traversal(root)
+	print
 
 	l = []
 	# define a lambda function that collates individual node values into a list
 	collate_fn = lambda kwargs, data : kwargs['lst'].append(data.value)
 	bst.preorder_traversal(root, collate_fn, lst=l)
 	assert (l == ['+', 'a', '*', 'b', 'c'])
+
+	print 'Postorder: ',
+	bst.postorder_traversal(root)
+	print
+
+	l = []
+	bst.postorder_traversal(root, collate_fn, lst=l)
+	assert (l == ['a',  'b', 'c', '*', '+'])
+
+	print 'Inorder: ',
+	bst.inorder_traversal(root)
+	print
+
+	l = []
+	bst.inorder_traversal(root, collate_fn, lst=l)
+	assert (l == ['a', '+', 'b', '*', 'c'])
+
+
+if __name__ == "__main__":
+	test_traversals()
+
