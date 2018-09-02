ef30b1c807ac70590181598316d4884647a2973f
Niranjan Nagaraju <vinithepooh@gmail.com>
2018-09-02 22:54:57 +0530

    Refactor SLL node for C++ implementations, build system works for node subdirectory

--- c++/Data_Structures/sll/node/src/sll_node.cpp
+++ c++/Data_Structures/sll/node/src/sll_node.cpp
@@ -1,19 +1,21 @@
-#include <sll_node>
-using namespace std;
+#include <sll_node.h>
+//using namespace std;
 
 sll_node::sll_node(void *data) 
 {
-	this->_node = sll_node_create(data);
+	//this->_node = sll_node_create(data);
 }
 
 
 sll_node::~sll_node()
 {
-	this->_node = sll_node_delete(this->_node);
+	//this->_node = sll_node_delete(this->_node);
 }
 
+/*
 void 
 sll_node::print (printfn *printer)
 {
 	sll_node_print(this->_node, printer);
 }
+*/
