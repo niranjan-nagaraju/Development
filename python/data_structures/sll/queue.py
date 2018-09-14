710d358fa7e6fe10bf0bec6eea6542c972e5cd91
Niranjan Nagaraju <vinithepooh@gmail.com>
2018-09-14 16:36:34 +0530

    Refactor SLL, add a queue implementation based on SLL

---
+++ python/data_structures/sll/queue.py
@@ -0,0 +1,42 @@
+from sll import *
+
+# Queue using a DLL
+class Queue:
+	def __init__(self):
+		self.sll = SLL()
+
+	def size(self):
+		return self.sll.size
+
+	# enqueue and return DLL node (will be the tail)
+	def enqueue(self, value):
+		self.sll.enqueue(value)
+
+
+	# dequeue and return the DLL node (will be the previous head)
+	def dequeue(self):
+		return self.sll.dequeue()
+
+
+	def __str__(self):
+		return self.sll.__str__()
+
+
+
+
+if __name__ == "__main__":
+	queue = Queue()
+
+	for i in range(1, 6):
+		queue.enqueue(i)
+
+	assert(queue.size() == 5)
+
+	print 'Queue after enqueues', queue
+
+	for i in range(1, 6):
+		assert(i == queue.dequeue())
+		assert(queue.size() == (5-i))
+
+	print 'Queue testcases passed'
+
--- python/data_structures/sll/sll.py
+++ python/data_structures/sll/sll.py
@@ -1,13 +1,31 @@
 from node import *
 
+# Exceptions for the SLL class
+class OverFlowError(Exception):
+	''' List Overflow '''
+	pass
+
+class UnderFlowError(Exception):
+	''' List Underflow '''
+	pass
+
+
+
 class SLL:
 	def __init__(self):
 		self.head = None
 		self.tail = None
 		self.size = 0
 
+		# function aliases for better readability
+		self.enqueue = self.push_back
+		self.dequeue = self.pop_front
+		self.push = self.push_back
+		self.pop = self.pop_back
+
+
 	# Insert at front
-	def insert(self, value):
+	def push_front(self, value):
 		node = Node(value)
 		self.size += 1
 
@@ -17,8 +35,9 @@ class SLL:
 		if (not self.tail):
 			self.tail = node
 
-	# Append to the rear
-	def append(self, value):
+
+	# Insert to the rear
+	def push_back(self, value):
 		node = Node(value)
 
 		self.size += 1
@@ -31,6 +50,48 @@ class SLL:
 
 		self.tail.next = node
 		self.tail = node
+
+
+	# remove first element from the SLL
+	def pop_front(self):
+		if not self.head:
+			raise UnderFlowError
+			return None
+
+		self.size -= 1
+		value = self.head.value
+		self.head = self.head.next
+		return value
+
+
+	# remove last element from the SLL	
+	def pop_back(self):
+		if not self.head:
+			raise UnderFlowError
+			return None
+
+		self.size -= 1
+		value = self.tail.value
+
+		# There was only 1 element in the SLL
+		if self.size == 0:
+			self.head = self.tail = None
+			return value
+
+		trav = self.head
+
+		# traverse until we reach the penultimate node in the SLL
+		while trav.next != self.tail:
+			trav = trav.next
+
+		# make penultimate node the new tail, and cut old tail from its link
+		trav.next = None
+		self.tail = trav
+
+		return value
+
+
+
 
 
 	def __str__(self):
@@ -53,8 +114,6 @@ class SLL:
 		while second:
 			third = second.next
 
-			print first, second, third
-
 			# Make second->first link
 			second.next = first
 
@@ -72,21 +131,77 @@ class SLL:
 		self.head = first
 
 
+
+def test_reverse():
+	sll = SLL()
+	sll.push_back(1)
+	sll.push_back(2)
+	sll.push_back(3)
+	sll.push_back(4)
+
+	print "Before reversing: sll", sll
+	sll.reverse()
+	print "After reversing: sll", sll
+
+
 def TC1():
 	sll = SLL()
-	sll.append(1)
-	sll.append(2)
-	sll.append(3)
-	sll.append(4)
+	sll.push_back(1)
+	assert(sll.size == 1)
+	assert(sll.head.value == 1) 
+	assert(sll.tail.value == 1) 
 
-	print sll
-	print sll.head, sll.tail
+	sll.push_front(0)
+	assert(sll.size == 2)
+	assert(sll.head.value == 0) 
+	assert(sll.tail.value == 1) 
 
-	sll.reverse()
-	print sll
-	print sll.head, sll.tail
 
+	sll.push_back(2)
+	assert(sll.size == 3)
+	assert(sll.head.value == 0) 
+	assert(sll.tail.value == 2) 
+
+	sll.enqueue(3)
+	assert(sll.size == 4)
+	assert(sll.head.value == 0) 
+	assert(sll.tail.value == 3) 
+
+	sll.push(4)
+	assert(sll.size == 5)
+	assert(sll.head.value == 0) 
+	assert(sll.tail.value == 4) 
+
+	assert(4 == sll.pop_back())
+	assert(sll.size == 4)
+	assert(sll.head.value == 0) 
+	assert(sll.tail.value == 3) 
+
+	assert(0 == sll.pop_front())
+	assert(sll.size == 3)
+	assert(sll.head.value == 1) 
+	assert(sll.tail.value == 3) 
+
+	assert(1 == sll.dequeue())
+	assert(sll.size == 2)
+	assert(sll.head.value == 2) 
+	assert(sll.tail.value == 3) 
+
+	assert(3 == sll.pop())
+	assert(sll.size == 1)
+	assert(sll.head.value == 2) 
+	assert(sll.tail.value == 2) 
+
+
+	assert(2 == sll.pop_back())
+	assert(sll.size == 0)
+	assert(sll.head == None) 
+	assert(sll.tail == None) 
+
+	print 'TC1 passed'
 
 
 if __name__ == "__main__":
 	TC1()
+	test_reverse()
+
