cced2e05f1d45f8e6946fc4a936ee3d2ee77a17b
Niranjan Nagaraju <vinithepooh@gmail.com>
2018-09-28 13:00:07 +0530

    Separate out node and data methods

--- python/data_structures/dll/queue.py
+++ python/data_structures/dll/queue.py
@@ -10,23 +10,37 @@ class Queue(DLL):
 	def frontNode(self):
 		return self.head
 
+	# return the item at the front of the Queue
 	def front(self):
 		return self.head.value
 
-	# Override enqueue and return DLL node (will be the tail)
+	# return the last node in the Queue
+	def lastNode(self):
+		return self.tail
+
+	# return the item at the end of the Queue
+	def last(self):
+		return self.tail.value
+
+	# Enqueue item at the back of the queue 
 	def enqueue(self, value):
 		self.push_back(value)
-		return self.tail
 
-
+	# Enqueue a 'DLL wrapped node' at the back of the queue 
 	def enqueueNode(self, node):
 		self.push_back(node)
-		return self.tail
 
-	# dequeue and return the DLL node (will be the previous head)
-	def dequeue(self):
+	# Dequeue node at front of the queue and return it (will be the previous head)
+	def dequeueNode(self):
 		return self.pop_front_node()
 
+	# Dequeue item at the front of the queue and return the item
+	def dequeue(self):
+		return self.pop_front()
+
+	# Remove specified node from the queue (wherever it is), 
+	# and enqueue it back at the end of the queue
+	# NOTE: No checks ar performed to see if the if 'node' indeed is part of the queue for optimization reasons.
 	def reEnqueueNode(self, node):
 		# if the node is already at the 
 		# back of the queue, 
