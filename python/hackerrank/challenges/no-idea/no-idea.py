f64ef14c6897bed6f142975dcdd699cd117d7185
Niranjan Nagaraju <vinithepooh@gmail.com>
2018-09-29 12:25:33 +0530

    Use closure to fix which set to add input elements onto, and pass the closed function to map

--- python/hackerrank/challenges/no-idea/no-idea.py
+++ python/hackerrank/challenges/no-idea/no-idea.py
@@ -1,5 +1,13 @@
 '''
 https://www.hackerrank.com/challenges/no-idea/problem
+
+A, B are two _disjoint_ sets with lengths n, and m respectively
+for every element x in input array 'arr', 
+   if x is in A, add 1 to happiness score
+   if x is in B, subtract 1 from happiness score
+
+A and B are disjoint sets means A and B dont have any common elements between them
+A and B are sets => there are no duplicate elements within A or B
 '''
 n,m = map(int, raw_input().strip().split())
 arr = map(int, raw_input().strip().split())
@@ -8,11 +16,14 @@ arr = map(int, raw_input().strip().split())
 A = set()
 B = set()
 
-addToSetA = lambda x: A.add(int(x))
-addToSetB = lambda x: B.add(int(x))
+# Use closure functions to implement 'partial lambda functions' a la haskell
+def addToSet(in_set):
+	# return inner closed function that has 'set to be inserted into' closed
+	return lambda x: in_set.add(int(x))
 
-map(addToSetA, raw_input().strip().split())
-map(addToSetB, raw_input().strip().split())
+
+map(addToSet(A), raw_input().strip().split())
+map(addToSet(B), raw_input().strip().split())
 
 happiness = 0
 
@@ -26,6 +37,12 @@ print happiness
 
 
 '''
+[16:53:05 no-idea]$ cat no-idea.in
+3 2
+1 5 3
+3 1
+5 7
+
 [16:53:08 no-idea]$ cat no-idea.in | python no-idea.py 
 1
 '''
