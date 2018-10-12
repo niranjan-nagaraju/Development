eda5036a0ebd3cde64ba46eae8fd6af6a5e336b3
Niranjan Nagaraju <vinithepooh@gmail.com>
2018-10-12 19:42:58 +0530

    Add an iterative implementation of binary search, recursive version stays default

--- python/algorithms/search/binary_search/binary_search.py
+++ python/algorithms/search/binary_search/binary_search.py
@@ -2,7 +2,9 @@
 Binary search on a sorted list
 '''
 
-def binary_search(lst, x):
+
+# A Recursive binary search implementation
+def binary_search_r(lst, x):
 	# Find x in lst[l:h]
 	def binary_search_helper(lst, x, l, h):
 		# List's low and high indices have criss-crossed
@@ -14,8 +16,10 @@ def binary_search(lst, x):
 		if lst[mid] == x:
 			return True
 		elif x > lst[mid]:
+			# search in second half of the sub-array
 			return binary_search_helper(lst, x, mid+1, h)
 		else:
+			# search in first half of the sub-array
 			return binary_search_helper(lst, x, l, mid-1)
 
 
@@ -24,8 +28,30 @@ def binary_search(lst, x):
 
 
 
-# basic testcases
-if __name__ == "__main__":
+# Iterative implementation for binary search
+def binary_search_i(lst, x):
+	l, h = 0, len(lst)-1
+	while l <= h:
+		mid = (l+h)/2
+		if lst[mid] == x:
+			return True
+		elif x > lst[mid]:
+			# search in second half of the sub-array
+			l = mid+1
+		else:
+			# search in first half of the sub-array
+			h = mid-1
+
+	# Couldn't find 'x'
+	return False
+
+
+
+# If not explicity called, use recursive binary search as default
+binary_search = binary_search_r
+
+
+def test_recursive_version():
 	assert(binary_search([1,2,3,4,5], 2) == True)
 	assert(binary_search([1,2,3,4,5], 1) == True)
 	assert(binary_search([1,2,3,4,5], 5) == True)
@@ -33,7 +59,6 @@ if __name__ == "__main__":
 
 	assert(binary_search((1,2,3,4,5), 5) == True)
 	assert(binary_search((1,2,3,4,5), 0) == False)
-
 
 	assert(binary_search([2,4,6, 8], 2) == True)
 	assert(binary_search([2,4,6, 8], 4) == True)
@@ -43,3 +68,30 @@ if __name__ == "__main__":
 	assert(binary_search([2,4,6, 8], 3) == False)
 	assert(binary_search([2,4,6, 8], 5) == False)
 	assert(binary_search([2,4,6, 8], 7) == False)
+
+
+def test_iterative_version():
+	assert(binary_search_i([1,2,3,4,5], 2) == True)
+	assert(binary_search_i([1,2,3,4,5], 1) == True)
+	assert(binary_search_i([1,2,3,4,5], 5) == True)
+	assert(binary_search_i([1,2,3,4,5], 0) == False)
+
+	assert(binary_search_i((1,2,3,4,5), 5) == True)
+	assert(binary_search_i((1,2,3,4,5), 0) == False)
+
+	assert(binary_search_i([2,4,6, 8], 2) == True)
+	assert(binary_search_i([2,4,6, 8], 4) == True)
+	assert(binary_search_i([2,4,6, 8], 6) == True)
+	assert(binary_search_i([2,4,6, 8], 8) == True)
+	assert(binary_search_i([2,4,6, 8], 1) == False)
+	assert(binary_search_i([2,4,6, 8], 3) == False)
+	assert(binary_search_i([2,4,6, 8], 5) == False)
+	assert(binary_search_i([2,4,6, 8], 7) == False)
+
+
+
+# basic testcases
+if __name__ == "__main__":
+	test_recursive_version()
+	test_iterative_version()
+
