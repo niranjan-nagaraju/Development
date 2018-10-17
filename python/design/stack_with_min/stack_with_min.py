49c4a59f93d6094b8a20952613122583c7870e79
Niranjan Nagaraju <vinithepooh@gmail.com>
2018-10-17 21:05:57 +0530

    Describe approach to implementing stack with min()

--- python/design/stack_with_min/stack_with_min.py
+++ python/design/stack_with_min/stack_with_min.py
@@ -1,9 +1,39 @@
 '''
 A stack which supports min() in O(1) 
 in addition to the exisitng push() and pop() in O(1)
+
+min(): returns the current minimum in the stack
+
+e.g.
+push(5)
+push(4)
+push(3)
+min() -> returns 3
+pop() -> returns 3
+min() -> returns 4 as 4 is the new minimum
+push(1)
+min() -> returns 1
+
+Approach:
+	Over a sequence of push() and pop(), min() keeps changing => we need to keep the 'current min' as part of the stack entries.
+	Further, since a stack is a LIFO, and pop() returns newer entries, storing current min along with each push() would keep track of
+	relative min() as the stack grew.
+
+	push(5):
+	   stack: (5,5)
+	push(4):
+		stack: (5,5), (4,4)
+	push(3):
+		stack: (5,5), (4,4), (3,3)
+	min() -> return min from tos -> 3
+	pop() -> return (3,3)
+		stack: (5,5), (4,4) <- min() is automatically adjusted
+	push(1)
+		stack: (5,5), (4,4) (1,1)
+	min() -> return min from tos -> 1
 '''
 
-from data_structures.array.stack.stack import *
+from data_structures.array.stack.stack import Stack
 
 class StackMin(Stack):
 	def __init__(self, capacity=0):
@@ -26,7 +56,7 @@ class StackMin(Stack):
 		if self.size != 0:
 			self.min = self.items[self.tos][1]
 		else:
-			self.min = 0
+			self.min = None
 
 		return item
 
