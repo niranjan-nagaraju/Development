ec0b720648e62d8c91caa4ff989215251613fcbd
Niranjan Nagaraju <vinithepooh@gmail.com>
2018-10-30 16:53:49 +0530

    Queue using stacks - dequeue efficient version

--- python/design/queue_using_stacks/queue_using_stacks.py
+++ python/design/queue_using_stacks/queue_using_stacks.py
@@ -1,4 +1,171 @@
 '''
-PLACEHOLDER:
-	push-efficient one
+Implement a queue using stacks
+
+Approach:
+	An Dequeue-efficient implementation using two stacks
+	enqueue is O(n), dequeue is O(1)
+
+	Enqueue(x): 
+		Use S2 to reverse order in S1 -- push all items from S1 to S2
+		Add x to S1
+		Push all items from S2 into S1
+	Dequeue():
+		x = S1.pop() 
+
+	Test runs:
+		Enqueue(1):
+			S1: 1
+			S2:
+		 
+		Enqueue(2):
+			S1: 1
+			S2: 
+			<-- S1 -> S2
+			S1: 
+			S2: 1
+			<-- 
+			S1: 2
+			S2: 1
+			<--
+			S1: 1 2
+			S2:
+
+		Enqueue(3):
+			S1: 1 2
+			S2: 
+			<-- S1 -> S2
+			S1: 
+			S2: 2 1
+			<--
+			S1: 3
+			S2: 2 1
+			<--
+			S1: 2 3
+			S2: 1
+			<--
+			S1: 1 2 3
+			S2:
+
+		Dequeue(): 1
+			S1: 1 2 3
+			S2: 
+			<-- x = S1.pop() == 1
+			S1: 2 3
+			S2:
+
+		Enqueue(4)
+			S1: 2 3
+			S2:
+			<--
+			S1: 4
+			S2: 3 2
+			<--
+			S1: 2 3 4
+			S2:
+
+		Enqueue(5)
+			S1: 2 3 4
+			S2:
+			<--
+			S1: 5
+			S2: 4 3 2
+			<--
+			S1: 2 3 4 5
+			S2:
+
+		Dequeue(): 2
+		Dequeue(): 3
+		Dequeue(): 4
+		Dequeue(): 5
 '''	
+
+from data_structures.sll.stack import Stack
+
+
+class Queue(object):
+	def __init__(self):
+		self.s1 = Stack()
+		self.s2 = Stack()
+
+
+	def front(self):
+		return self.s1.top()
+
+
+	#NOTE: back() cannot be implemented efficiently
+	def back(self):
+		for x in self.s1:
+			pass
+
+		return x
+
+
+	def length(self):
+		return self.s1.length()
+
+
+	def enqueue(self, x):
+		# Helper function to move all items from stack a to stack b
+		def move(a, b):
+			while a.length():
+				b.push(a.pop())
+
+		# Use S2 to reverse S1's order
+		move(self.s1, self.s2)
+
+		self.s1.push(x)
+
+		# Move back S2 to S1, reversing it again, restoring S1's queue-order
+		move(self.s2, self.s1)
+
+
+
+	def dequeue(self):
+		return self.s1.pop()
+
+
+
+	def __str__(self):
+		return self.s1.__str__()
+
+
+	def __repr__(self):
+		return "%r %r" %(self.s1, self.s2)
+
+
+
+# Basic testcases
+if __name__ == "__main__":
+	queue = Queue()
+
+	for i in range(1, 4):
+		queue.enqueue(i)
+		assert(queue.front() == 1)
+		assert(queue.back() == i)
+
+	assert(queue.length() == 3)
+	assert(queue.dequeue() == 1)
+
+	queue.enqueue(4)
+	queue.enqueue(5)
+
+	print 'Queue after enqueues & dequeues', queue
+	print 'Queue repr %r' %(queue)
+
+	for i in range(2, 6):
+		assert(i == queue.dequeue())
+		assert(queue.length() == (5-i))
+
+
+	q2 = Queue()
+	q2.enqueue('+')
+	q2.enqueue('a')
+	assert('+' == q2.dequeue())
+	q2.enqueue('b')
+	assert('a' == q2.dequeue())
+	assert('b' == q2.dequeue())
+
+	print 'Queue testcases passed'
+
+
+
--- python/design/queue_using_stacks/queue_using_stacks2.py
+++ python/design/queue_using_stacks/queue_using_stacks2.py
@@ -1,4 +1,74 @@
 '''
-PLACEHOLDER:
-	pop-efficient one
+Implement a queue using stacks
+
+Approach:
+	An enqueue-efficient implementation using two stacks
+	Enqueue is O(1), dequeue is O(n)
+
+	Enqueue(x): 
+		Push to S1
+	Dequeue():
+		if S2 has elements (they are already in queue-order)
+		   return S2.pop()
+		Otherwise (S2 is empty)
+			Push all items from S1 to S2, S2 now has queue order
+			return S2.pop()
+		
+
+	Test runs:
+	Enqueue(1):
+		S1: 1
+		S2:
+	 
+	Enqueue(2):
+		S1: 2 1
+		S2: 
+
+	Enqueue(3):
+		S1: 3 2 1
+		S2: 
+
+	Dequeue(): 1
+	    S1: 3 2 1
+	    S2: 
+		<--
+		S1:
+		S2: 1 2 3
+		<-- x = S2.pop() == 1
+		S1: 
+		S2: 2 3
+
+	Enqueue(4)
+		S1: 4
+		S2: 2 3
+
+	Enqueue(5)
+		S1: 5 4
+		S2: 2 3
+
+	Dequeue() : 2
+		S1: 5 4
+		S2: 2 3
+		<-- S2.pop() == 2
+		S1: 5 4
+		S2: 3
+
+	Dequeue(): 3
+		S1: 5 4
+		S2: 3
+		<-- S2.pop() == 3
+		S1: 5 4
+		S2: 
+
+	Dequeue(): 4
+		S1: 5 4
+		S2: 
+		<-- 
+		S1: 
+		S2: 4 5
+
+	Dequeue(): 5
 '''	
+
+class QueueUsingStack(object):
+
