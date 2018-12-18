ec40028c060edbc0d0c3864e262776b6a6b7d1cd
Niranjan Nagaraju <vinithepooh@gmail.com>
2018-12-18 23:38:51 +0530

    O(n) solution to find if two strings are rotations of each other: Annotate code

--- python/algorithms/strings/rotations.py
+++ python/algorithms/strings/rotations.py
@@ -1,7 +1,19 @@
-# Find if two strings are rotations of each other
-# O(n) time complexity
+'''
+Find if two strings are rotations of each other
+O(n) time complexity
+
+Solution:
+	Append str1 to itself, then check if str2 is a substring within (str1+str1)
+'''
 
 def areRotations (str1, str2):
+	# Compare lengths of the two strings 
+	# to avoid false-positives
+	# Else, str2 can be a substring of str1,
+	#    e.g., str1: "abcad", str2: "cad"
+	# *OR* str1 a substring of str2
+	#    e.g., str1: "abcad", str2: "dabcad"
+	# and we might return True incorrectly
 	if len(str1) != len(str2):
 		return False
 
@@ -19,6 +31,8 @@ def main():
 	assert areRotations ("abc", "bca") == True
 	assert areRotations ("abc", "cab") == True
 	assert areRotations ("abc", "cba") == False
+	assert areRotations ("waterbottle", "erbottlewat") == True
+	assert areRotations("abcad", "cadab") == True
 
 if __name__ == "__main__":
 	main()
