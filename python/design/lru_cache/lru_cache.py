83637de2a570f0d8e26ae1a19c3bad4c99289aff
Niranjan Nagaraju <vinithepooh@gmail.com>
2019-01-21 14:37:56 +0530

    Add solution description to LRU cache implementation in python

--- python/design/lru_cache/lru_cache.py
+++ python/design/lru_cache/lru_cache.py
@@ -1,3 +1,46 @@
+'''
+Implement a LRU cache that supports the 'set()' and 'get()' operations.
+
+  + get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+
+  + put(key, value) - Set or insert the value if the key is not already present. 
+    When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item. 
+
+  + get(), set() are O(1)
+
+Solution:
+	Use a hash-table that associates cache lookup keys to nodes in a queue based on a doubly linked list.
+	The doubly linked list/Queue nodes contain (key, value) ordered by their recent usage.
+
+	+ get(key): find key in hash-table, get the corresponding node in the DLL.
+	  fetch node.value to return, since 'key' was recently used, 
+	  re-enqueue 'node' back to the back of the DLL/queue.
+	  NOTE: The queue is based on a DLL instead of a SLL, so re-enqueues are efficient.
+
+	+ set(key, value):
+      if the cache has not reached capacity,
+	  add node(key,value) into the queue at the end. 
+	  map key to node in the hash-table.
+	  Otherwise, 
+	    if the cache has reached capacity -> time to flush the least recently entry in the cache.
+		the least recently used 'key_' is the one at the front of the DLL/queue.
+		dequeue it, remove said 'key_' from the hash-table as well.
+		Now, add new (key,value) into the cache as usual.
+
++---------------------------+
+|      +---+                |
+|      | a +--------+       |
+|      +---+        |       |
++------+ b |      +-v-+   +-v-+   +---+  DLL/Q
+       +---+      | a <---+ b <---+ c |
+       | c |      | a'+---> b'+---> c'|
+   H/T +-+-+      +---+   +---+   ++--+
+         |                         ^
+         |                         |
+         +-------------------------+
+
+'''
+
 import sys
 sys.path.append("../../")
 from data_structures.dll.queue import Queue
