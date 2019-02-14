489c26a0675673f47e9c40a43b93e08804a5fd2f
Niranjan Nagaraju <vinithepooh@gmail.com>
2019-02-14 11:56:36 +0530

    Graph: placeholder

--- python/data_structures/graph/graph.py
+++ python/data_structures/graph/graph.py
@@ -1,3 +1,26 @@
 
-class Graph:
-	pass
+class Graph(object):
+	def __init__(self, rows=1, columns=1):
+		self.rows = rows
+		self.columns = columns
+		self._matrix = [[]]
+		self._adj_list = []
+
+	
+	@property
+	def matrix(self):
+		return self._matrix 
+
+
+	@property
+	def adj_list(self):
+		return self._adj_list 
+
+
+
+	def bfs(self):
+		pass
+
+
+	def dfs(self):
+		pass
