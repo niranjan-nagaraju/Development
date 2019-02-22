dd4b551d9ea28cff4336a12b503d07316dd07446
Niranjan Nagaraju <vinithepooh@gmail.com>
2019-02-22 16:39:53 +0530

    Test unpacking *args and **kwargs in python3

---
+++ python/snippets/unpacking_args_kwargs.py
@@ -0,0 +1,39 @@
+'''
+Test program to test *args and **kwargs unpacking in function calls
+'''
+
+
+def f1(a, b, c):
+	return a+b+c
+
+
+def f2(a):
+	return a+10
+
+
+
+def f3():
+	return 100
+
+
+
+def handler(fn, *args, **kwargs):
+	return fn(*args, **kwargs)
+
+
+
+assert(handler(f1, 1, 2, 3) ==  6)
+assert(handler(f2, 3) ==  13)
+assert(handler(f3) ==  100)
+
+t = (2,3,5)
+assert((f1(*t) == 10))
+
+p = [6,4,3]
+assert(f1(*p) == 13)
+
+assert(f2(*[1]) == 11)
+assert(f3(*[]) == 100)
+
+
+assert(handler(f1, c="3", b="2", a="1") == "123")
