8876e3bf8deb2a09644d4e682ab57afe539ccd5d
Niranjan Nagaraju <vinithepooh@gmail.com>
2019-02-26 15:47:30 +0530

    Unpack **kwargs

--- python/snippets/unpacking_args_kwargs.py
+++ python/snippets/unpacking_args_kwargs.py
@@ -1,3 +1,5 @@
+#!/usr/local/bin/python3
+
 '''
 Test program to test *args and **kwargs unpacking in function calls
 '''
@@ -21,19 +23,24 @@ def handler(fn, *args, **kwargs):
 	return fn(*args, **kwargs)
 
 
+if __name__ == '__main__':
+	assert(handler(f1, 1, 2, 3) ==  6)
+	assert(handler(f2, 3) ==  13)
+	assert(handler(f3) ==  100)
 
-assert(handler(f1, 1, 2, 3) ==  6)
-assert(handler(f2, 3) ==  13)
-assert(handler(f3) ==  100)
+	t = (2,3,5)
+	assert((f1(*t) == 10))
 
-t = (2,3,5)
-assert((f1(*t) == 10))
+	p = [6,4,3]
+	assert(f1(*p) == 13)
 
-p = [6,4,3]
-assert(f1(*p) == 13)
+	assert(f2(*[1]) == 11)
+	assert(f3(*[]) == 100)
 
-assert(f2(*[1]) == 11)
-assert(f3(*[]) == 100)
+	assert(handler(f1, c="3", b="2", a="1") == "123")
 
+	d = {'a': 1, 'c': 4, 'b': 2}
+	assert(handler(f1, **d) == 7)
+	x = {'a': 1}
+	assert(handler(f2, **x) == 11)
 
-assert(handler(f1, c="3", b="2", a="1") == "123")
