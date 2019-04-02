7ecfd5003e840ca7836b52c12b9fe39366693e0f
Niranjan Nagaraju <vinithepooh@gmail.com>
2019-04-02 19:29:13 +0530

    Powerset: Use subset markers based on array indices, and then fill in items from the set at these indices; convert subsets to string if the original set was a string

--- python/algorithms/combinatorics/powersets/powerset.py
+++ python/algorithms/combinatorics/powersets/powerset.py
@@ -37,8 +37,24 @@ def generate_power_set(a, n):
 	# Initialize an empty 2D list to store subsets ordered by length
 	# Lengths of subsets vary from 0 to n
 	powerset = [[] for i in xrange(n+1)]
+	subsets_selector = range(n)
 	for i in xrange(n):
-		generate_power_set_helper (powerset, a[i:], [], 0, n-i)
+		generate_power_set_helper (powerset, subsets_selector[i:], [], 0, n-i)
+
+	# Fill in actual items from the original set based on the subset masks
+	for i in range(n+1):
+		for x in powerset[i]:
+			for k in range(i):
+				x[k] = a[x[k]]
+
+	# If the original set was a string, stringify the subsets too
+	if isinstance(a, str):
+		powerset_str = [[] for i in xrange(n+1)]
+		powerset_str[0] = ""
+		for i in range(1,n+1):
+			for x in powerset[i]:
+				powerset_str[i].append(''.join(x))
+		return powerset_str
 
 	return powerset
 
@@ -91,14 +107,13 @@ def basic_testcases():
 	assert subsets[5] == []
 
 
-	a = range(1, 6)
-	powerset = generate_power_set(a, len(a))
-	assert(powerset == [[],
-		[[1], [2], [3], [4], [5]],
-		[[1, 2], [1, 3], [1, 4], [1, 5], [2, 3], [2, 4], [2, 5], [3, 4], [3, 5], [4, 5]],
-		[[1, 2, 3], [1, 2, 4], [1, 2, 5], [1, 3, 4], [1, 3, 5], [1, 4, 5], [2, 3, 4], [2, 3, 5], [2, 4, 5], [3, 4, 5]],
-		[[1, 2, 3, 4], [1, 2, 3, 5], [1, 2, 4, 5], [1, 3, 4, 5], [2, 3, 4, 5]],
-		[[1, 2, 3, 4, 5]]])
+	a = "abcde"
+	assert(generate_power_set(a, len(a)) == ['', ['a', 'b', 'c', 'd', 'e'], ['ab', 'ac', 'ad', 'ae', 'bc', 'bd', 'be', 'cd', 'ce', 'de'], ['abc', 'abd', 'abe', 'acd', 'ace', 'ade', 'bcd', 'bce', 'bde', 'cde'], ['abcd', 'abce', 'abde', 'acde', 'bcde'], ['abcde']])
+
+	assert generate_power_set("abc", 3) == ['', ['a', 'b', 'c'], ['ab', 'ac', 'bc'], ['abc']]
+	assert generate_power_set(('a', 'b', 'c'), 3) == [[], [['a'], ['b'], ['c']], [['a', 'b'], ['a', 'c'], ['b', 'c']], [['a', 'b', 'c']]]
+
+	assert generate_power_set([1,3,5,7], 4) == [[], [[1], [3], [5], [7]], [[1, 3], [1, 5], [1, 7], [3, 5], [3, 7], [5, 7]], [[1, 3, 5], [1, 3, 7], [1, 5, 7], [3, 5, 7]], [[1, 3, 5, 7]]]
 
 
 if __name__ == "__main__":
