4aae53975eefcc32b1069b1c304ecc266ad4f543
Niranjan Nagaraju <vinithepooh@gmail.com>
2019-04-27 16:02:10 +0530

    Implement decreaseKey(); testcases for sorted()

--- python/data_structures/heap/testcases/test_heap.py
+++ python/data_structures/heap/testcases/test_heap.py
@@ -62,10 +62,44 @@ def test_build_heap():
 	assert l == [0, 1, 4, 2, 6, 5, 8, 3, 7, 9, 10]
 
 
+
+def test_decreaseKey():
+	l = [0, 1, 4, 2, 6, 5, 8, 3, 7, 9, 10]
+	h = Heap()
+	h.items = l
+
+	caught_exception = False
+	try:
+		h.decreaseKey(4, 7)
+	except ValueError as v:
+		assert v.message == "ValueError: decreaseKey() - New key should be less current value"
+		caught_exception = True
+	assert caught_exception == True
+
+	h.decreaseKey(4, 0)
+	assert h.items == [0, 0, 4, 2, 1, 5, 8, 3, 7, 9, 10]
+
+
+def test_sorted():
+	l = range(10, -1, -1)
+	Heap.build_heap(l)
+	h = Heap()
+	h.items = l
+
+	new_l = []
+	h.sorted(lambda i, l: l.append(i), new_l)
+
+	# TODO: Fix assert fail here owing to bug in remove()
+	# when bubble_down() is incorrect due to one/both of L-R not present
+	assert new_l == range(11)
+
+
 def basic_testcases():
 	test_add()
 	test_remove()
 	test_build_heap()
+	test_decreaseKey()
+	test_sorted()
 
 if __name__ == '__main__':
 	basic_testcases()
