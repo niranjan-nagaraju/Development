b2eed7cc174d91248b22badfd6b4833424da76b4
Niranjan Nagaraju <vinithepooh@gmail.com>
2019-08-30 18:05:01 +0530

    LIS: Dynamic programming solution - separate testcases for LIS dp table, and the LIS function itself

--- python/algorithms/LIS/LIS_dp.py
+++ python/algorithms/LIS/LIS_dp.py
@@ -11,7 +11,6 @@ Solution:
       LIS[i] = max(LIS[i], LIS[j]), j: 0 -> i-1
    LIS: max(LIS[])
 
-TODO:
    The sequence(atleast one) itself can be got by getting a[x], a[y],... a[z]
 	  where x: rightmost index of max(LIS[]), y: rightmost index of max(LIS[])-1), ...., rightmost index of 1 
 '''
@@ -24,13 +23,18 @@ def rfind(arr, x, rightWin):
 
 	return -1
 
-def longest_increasing_subsequence(numbers):
+
+# Create a table of LIS for i:0 -> n-1, using dynamic programming
+def make_lis_table(numbers):
 	LIS_table = [1] * len(numbers)
 	for i in range(1, len(numbers)):
 		for j in range(0, i):
 			if numbers[i] > numbers[j]:
 				LIS_table[i] = max(LIS_table[i], LIS_table[j]+1)
+	return LIS_table
 	
+def longest_increasing_subsequence(numbers):
+	LIS_table = make_lis_table(numbers)
 	lis_len = max(LIS_table)
 
 	LIS = []
@@ -41,12 +45,18 @@ def longest_increasing_subsequence(numbers):
 		LIS.insert(0, numbers[idx])
 		curr_lis_len -= 1
 
-	return lis_len, LIS, LIS_table
+	return lis_len, LIS
 
 
 
 if __name__ == "__main__":
-	assert longest_increasing_subsequence([10,22,9,33,21,50,41,60]) == (5, [10, 22, 33, 41, 60], [1, 2, 1, 3, 2, 4, 4, 5])
-	assert longest_increasing_subsequence([5,4,3,2,1]) == (1, [1], [1, 1, 1, 1, 1])
-	assert longest_increasing_subsequence([1,2,3,4,5,4,3,2,1]) == (5, [1, 2, 3, 4, 5], [1, 2, 3, 4, 5, 4, 3, 2, 1])
-	assert longest_increasing_subsequence([10, 9, 2, 5, 3, 7, 101, 18]) == (4, [2,3,7,18], [1, 1, 1, 2, 2, 3, 4, 4])
+	assert make_lis_table([10,22,9,33,21,50,41,60]) == [1, 2, 1, 3, 2, 4, 4, 5]
+	assert make_lis_table([5,4,3,2,1]) == [1, 1, 1, 1, 1]
+	assert make_lis_table([1,2,3,4,5,4,3,2,1]) == [1, 2, 3, 4, 5, 4, 3, 2, 1]
+	assert make_lis_table([10, 9, 2, 5, 3, 7, 101, 18]) == [1, 1, 1, 2, 2, 3, 4, 4]
+
+	assert longest_increasing_subsequence([10,22,9,33,21,50,41,60]) == (5, [10, 22, 33, 41, 60])
+	assert longest_increasing_subsequence([5,4,3,2,1]) == (1, [1])
+	assert longest_increasing_subsequence([1,2,3,4,5,4,3,2,1]) == (5, [1, 2, 3, 4, 5])
+	assert longest_increasing_subsequence([10, 9, 2, 5, 3, 7, 101, 18]) == (4, [2,3,7,18])
+
