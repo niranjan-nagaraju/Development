df5a1fe4249735fa8617dba9b691791c8ca70a46
Niranjan Nagaraju <vinithepooh@gmail.com>
2019-09-14 21:44:33 +0530

    Binary Tree: Iterative postorder and preorder traversals; Use setUp() in unittests to initialize test binary trees

--- python/data_structures/trees/binary_tree/iterative_traversals.py
+++ python/data_structures/trees/binary_tree/iterative_traversals.py
@@ -17,7 +17,6 @@ class IterativeTraversals(object):
 		2. For each subtree rooted at root,
 			push root.left to the stack if root.left is not 'done' yet.
 			Else, pop and visit root, and push root.right to stack
-		   if node's left is empty or not 'done' yet.
 		3. Repeat until stack is empty, by examining stack top, and popping as necessary
 
 	Sample run:
@@ -104,10 +103,211 @@ class IterativeTraversals(object):
 				# current node's left is done
 				# pop it off the stack and
 				# visit it
+				# and then push current node's right child
 				s.pop()
 				visited[curr] = True
 				aggregate_fn(kwargs, curr.value)
 				s.push(curr.right) if curr.right else None
 			else:
 				s.push(curr.left)
+
+
+	'''
+	Postorder traversal of a binary tree (iterative version)
+	Algorithm outline:
+		0. Use a stack to store root nodes until the left and right subtrees are all printed.
+		1. Start with pushing the root node to the stack.
+		2. For each subtree rooted at root,
+			push root.right followed by root.left to the stack if either are not 'done' yet.
+			Else, pop and visit root
+		   NOTE: We push right, followed by left child so the stack ordering becomes left, right	
+		3. Repeat until stack is empty, by examining stack top, and popping as necessary
+
+	Sample run:
+                                1
+                              /   \	
+                             2     3
+                           /  \  /  \
+                          4   5 6    7
+
+		node 1:
+		Stack: [1]
+		visited: []
+
+		top == 1
+		1.right == 3 is not visited,
+		push 3 to stack
+		stack: [3,1]
+		1.left == 2 is not visited,
+		push 2 to stack
+		stack: [2,3,1]
+
+		top == 2
+		2.right == 5 is not visited,
+		push 5 to stack
+		stack: [5,2,3,1]
+		2.left == 4 is not visited
+		push 4 to stack
+		stack: [4,5,2,3,1]
+
+		top == 4
+		4.right is empty
+		4.left is empty
+		=> pop 4, visit 4
+		visited: [4]
+		Postorder traversal: [4]
+		stack: [5,2,3,1]
+
+		top == 5
+		5.right is empty
+		5.left is empty
+		=> pop 5, visit 5
+		visited: [4,5]
+		Postorder traversal: [4,5]
+		stack: [2,3,1]
+
+		top == 2
+		2.right == 5 is done
+		2.left  == 4 is done
+		=> pop 2, visit 2
+		visited: [4,5,2]
+		Postorder traversal: [4,5,2]
+		stack: [3,1]
+
+		top == 3
+		3.right == 7 is not done
+		push 7
+		stack: [7,3,1]
+		3.left  == 6 is not done
+		push 6
+		stack: [6,7,3,1]
+
+		top == 6
+		6.right is empty
+		6.left  is empty
+		=> pop 2, visit 2
+		stack: [7,3,1]
+		visited: [4,5,2,6]
+		Postorder traversal: [4,5,2,6]
+
+		top == 7
+		7.right is empty
+		7.left  is empty
+		=> pop 7, visit 7
+		stack: [3,1]
+		visited: [4,5,2,6,7]
+		Postorder traversal: [4,5,2,6,7]
+
+		top == 3
+		3.right == 7 is done
+		3.left == 6 is done
+		=> pop 3, visit 3
+		stack: [1]
+		visited: [4,5,2,6,7,3]
+		Postorder traversal: [4,5,2,6,7,3]
+
+		top == 1
+		1.right == 3 is done
+		1.left == 2 is done
+		=> pop 1, visit 1
+		stack: []
+		visited: [4,5,2,6,7,3,1]
+		Postorder traversal: [4,5,2,6,7,3,1]
+
+		Postorder traversal: [4,5,2,6,7,3,1]
+	'''
+	@staticmethod
+	def postorder_traversal(btree, aggregate_fn=_default_printfn, **kwargs):
+		if not btree or not btree.root:
+			return
+
+		s = Stack()
+		s.push(btree.root)
+		visited = defaultdict(bool)
+
+		# Helper function to check if a node is 'done' or not
+		isDone = lambda node: True if (node is None or visited[node]) else False
+		while s:
+			curr = s.top()
+			if isDone(curr.right) and isDone(curr.left):
+				# current node's left and right are done
+				# pop it off the stack and
+				# visit it
+				s.pop()
+				visited[curr] = True
+				aggregate_fn(kwargs, curr.value)
+			else:
+				s.push(curr.right) if curr.right else None
+				s.push(curr.left) if curr.left else None
+
+
+
+	'''
+	Postorder traversal of a binary tree (iterative version)
+	Algorithm outline:
+		0. A preorder traversal is a regular DFS, so use a stack to store child nodes
+		   after the root has been visited.
+		1. push root
+		2. pop stack, push root's left and right chidren
+		3. Repeat until stack is empty
+
+	Sample run:
+                                1
+                              /   \	
+                             2     3
+                           /  \  /  \
+                          4   5 6    7
+
+		node 1:
+		Stack: [1]
+
+		pop(1)
+		Preorder traversal: [1]
+		push(3)
+		push(2)
+		Stack: [2,3]
+
+		pop(2)
+		Preorder traversal: [1,2]
+		push(5)
+		push(4)
+		Stack: [4,5,3]
+
+		pop(4)
+		Preorder traversal: [1,2,4]
+		Stack: [5,3]
+
+		pop(5)
+		Preorder traversal: [1,2,4,5]
+		Stack: [3]
+
+		pop(3)
+		Preorder traversal: [1,2,4,5,3]
+		push(7)
+		push(6)
+		Stack: [6,7]
+
+		pop(6)
+		Preorder traversal: [1,2,4,5,3,6]
+		Stack: [7]
+
+		pop(7)
+		Preorder traversal: [1,2,4,5,3,6,7]
+		Stack: []
+
+		Preorder traversal: [1,2,4,5,3,6,7]
+	'''
+	@staticmethod
+	def preorder_traversal(btree, aggregate_fn=_default_printfn, **kwargs):
+		if not btree or not btree.root:
+			return
+
+		s = Stack()
+		s.push(btree.root)
+		while s:
+			curr = s.pop()
+			aggregate_fn(kwargs, curr.value)
+			s.push(curr.right) if curr.right else None
+			s.push(curr.left) if curr.left else None
+
 
--- python/data_structures/trees/binary_tree/testcases/test_iterative_traversals.py
+++ python/data_structures/trees/binary_tree/testcases/test_iterative_traversals.py
@@ -5,8 +5,8 @@ import unittest
 
 
 class Test_IterativeTraversals(unittest.TestCase):
-	def test_inorder_traversal(self):
-		aggregate_list = lambda kwargs, data : kwargs['lst'].append(data)
+	def setUp(self):
+		self.aggregate_list = lambda kwargs, data : kwargs['lst'].append(data)
 		'''
       1
     /   \
@@ -15,13 +15,7 @@ class Test_IterativeTraversals(unittest.TestCase):
 4    5 6    7
 		'''
 		l = [1,2,3,4,5,6,7]
-		bt = TreeMaker.fromList(l)
-
-		inorder = []
-		IterativeTraversals.inorder_traversal(bt, aggregate_list, lst=inorder)
-		inorder_r = []
-		bt.inorder_traversal(aggregate_list, lst=inorder_r)
-		assert inorder == inorder_r == [4,2,5,1,6,3,7]
+		self.bt = TreeMaker.fromList(l)
 
 		'''
       1
@@ -30,13 +24,7 @@ class Test_IterativeTraversals(unittest.TestCase):
  /   \
 4     5
 		'''
-		bt = TreeMaker.fromList([1, 2, None, 4, 5])
-		inorder = []
-		IterativeTraversals.inorder_traversal(bt, aggregate_list, lst=inorder)
-		inorder_r = []
-		bt.inorder_traversal(aggregate_list, lst=inorder_r)
-		assert inorder == inorder_r == [4,2,5,1]
-
+		self.bt2 = TreeMaker.fromList([1, 2, None, 4, 5])
 
 		'''
         1
@@ -47,13 +35,76 @@ class Test_IterativeTraversals(unittest.TestCase):
  /             \
 6               7
 		'''
-		bt = TreeMaker.fromList([1, 2, 3, 4, None, None, 5, 6, None, None, None, None, None, None, 7])
+		self.bt3 = TreeMaker.fromList([1, 2, 3, 4, None, None, 5, 6, None, None, None, None, None, None, 7])
+
+
+	def test_inorder_traversal(self):
+		bt = self.bt
 		inorder = []
-		IterativeTraversals.inorder_traversal(bt, aggregate_list, lst=inorder)
+		IterativeTraversals.inorder_traversal(bt, self.aggregate_list, lst=inorder)
 		inorder_r = []
-		bt.inorder_traversal(aggregate_list, lst=inorder_r)
+		bt.inorder_traversal(self.aggregate_list, lst=inorder_r)
+		assert inorder == inorder_r == [4,2,5,1,6,3,7]
+
+		bt = self.bt2
+		inorder = []
+		IterativeTraversals.inorder_traversal(bt, self.aggregate_list, lst=inorder)
+		inorder_r = []
+		bt.inorder_traversal(self.aggregate_list, lst=inorder_r)
+		assert inorder == inorder_r == [4,2,5,1]
+
+		bt = self.bt3
+		inorder = []
+		IterativeTraversals.inorder_traversal(bt, self.aggregate_list, lst=inorder)
+		inorder_r = []
+		bt.inorder_traversal(self.aggregate_list, lst=inorder_r)
 		assert inorder == inorder_r == [6,4,2,1,3,5,7]
 
+
+	def test_postorder_traversal(self):
+		bt = self.bt
+		postorder = []
+		IterativeTraversals.postorder_traversal(bt, self.aggregate_list, lst=postorder)
+		postorder_r = []
+		bt.postorder_traversal(self.aggregate_list, lst=postorder_r)
+		assert postorder == postorder_r == [4,5,2,6,7,3,1]
+
+		bt = self.bt2
+		postorder = []
+		IterativeTraversals.postorder_traversal(bt, self.aggregate_list, lst=postorder)
+		postorder_r = []
+		bt.postorder_traversal(self.aggregate_list, lst=postorder_r)
+		assert postorder == postorder_r == [4,5,2,1]
+
+		bt = self.bt3
+		postorder = []
+		IterativeTraversals.postorder_traversal(bt, self.aggregate_list, lst=postorder)
+		postorder_r = []
+		bt.postorder_traversal(self.aggregate_list, lst=postorder_r)
+		assert postorder == postorder_r == [6,4,2,7,5,3,1]
+
+
+	def test_preorder_traversal(self):
+		bt = self.bt
+		preorder = []
+		IterativeTraversals.preorder_traversal(bt, self.aggregate_list, lst=preorder)
+		preorder_r = []
+		bt.preorder_traversal(self.aggregate_list, lst=preorder_r)
+		assert preorder == preorder_r == [1,2,4,5,3,6,7]
+
+		bt = self.bt2
+		preorder = []
+		IterativeTraversals.preorder_traversal(bt, self.aggregate_list, lst=preorder)
+		preorder_r = []
+		bt.preorder_traversal(self.aggregate_list, lst=preorder_r)
+		assert preorder == preorder_r == [1,2,4,5]
+
+		bt = self.bt3
+		preorder = []
+		IterativeTraversals.preorder_traversal(bt, self.aggregate_list, lst=preorder)
+		preorder_r = []
+		bt.preorder_traversal(self.aggregate_list, lst=preorder_r)
+		assert preorder == preorder_r == [1,2,4,6,3,5,7]
 
 
 if __name__ == '__main__':
