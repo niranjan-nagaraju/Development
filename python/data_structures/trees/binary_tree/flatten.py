f4261b953f87a078652641a5cb87b6fc8c5ed1ec
Niranjan Nagaraju <vinithepooh@gmail.com>
2019-09-16 14:50:23 +0530

    Binary Tree: Flatten binary tree to a DLL in-place with nodes ordered like in an inorder traversal of the tree

---
+++ python/data_structures/trees/binary_tree/flatten.py
@@ -0,0 +1,77 @@
+from data_structures.trees.binary_tree.binary_tree import BinaryTree
+
+class FlattenBinaryTree(object):
+	'''
+	Flatten a Binary Tree into a doubly linked list in-place such that the DLL has nodes in the inorder traversal of the binary tree
+
+	Algorithm:
+		1. Recursively convert left and right subtrees to DLLs, each with their own head and tail.
+		2. Insert root in between the two DLLs merging the two DLLs into one.
+		   (lh, lt): represents left subtree's head and tail nodes
+		   (rh, rh): represents right subtree's head and tail nodes
+		   the new merged DLL will look like
+		   lh ... lt, root, rh, ... rh
+		   (lh, rt) will be the head and tail of the merged DLL
+		3. A leaf node's DLL (head, tail) will be itself
+
+	Sample run
+		 1
+	   /   \
+	  2     3
+	 / \   /  \
+	4   5 6   7
+	expected: 4 <-> 2 <-> 5 <-> 1 <-> 6 <-> 3 <-> 7
+
+	f(1):
+	    > f(2):
+	       >> f(4):
+		      leaf node => DLL: (4,4)   4
+		   Add 2 to DLL's tail => DLL: (4, 2)   4 <-> 2
+		   >> f(5)
+		      leaf node => DLL: (5,5)   5
+		   Merge (4,2) and (5,5)
+		   DLL: (4,5)  4 <-> 2 <-> 5
+		Add 1 to DLL's tail => DLL: (4,1)  4 <-> 2 <-> 5 <-> 1
+	    > f(3):
+	       >> f(6):
+		      leaf node => DLL: (6,6)   6
+		   Add 3 to DLL's tail => DLL: (6, 3)   6 <-> 3
+	       >> f(7):
+		      leaf node => DLL: (7,7)   7
+		   Merge (6,3) and (7,7)
+		   DLL: (6,7)  6 <-> 3 <-> 7
+		Merge left DLL = (4,1)  4 <-> 2 <-> 5 <-> 1, and right DLL = (6,7) 6 <-> 3 <-> 7
+		Final DLL: (4,7)  4 <-> 2 <-> 5 <-> 1 <-> 6 <-> 3 <-> 7
+	'''
+	@staticmethod
+	def binTreeToDLL_in(bt):
+		def _binaryTreeToDLL_in(root):
+			if root == None:
+				return (None, None)
+			
+			(lh, lt) = _binaryTreeToDLL_in(root.left)
+
+			# Merge current subtree's root to left subtree's DLL's tail
+			if lh:
+				lt.right = root
+				root.left = lt
+				lt = root
+			else: # Left subtree is empty
+				lh = lt = root
+
+			(rh, rt) = _binaryTreeToDLL_in(root.right)
+			# Merge (left+root) and right subtrees-based DLLs
+			if rh:
+				lt.right = rh
+				rh.left = lt
+			else: # right subtree is empty, return (left+root) DLL as-is
+				rt = lt
+
+			return (lh, rt)
+
+		# Call the helper function
+		if not bt:
+			return (None, None)
+		return _binaryTreeToDLL_in(bt.root)
+				
+
---
+++ python/data_structures/trees/binary_tree/testcases/test_flatten.py
@@ -0,0 +1,107 @@
+from data_structures.trees.binary_tree.binary_tree import BinaryTree
+from data_structures.trees.binary_tree.flatten import FlattenBinaryTree
+from data_structures.trees.binary_tree.tree_maker import TreeMaker
+import unittest
+
+
+class Test_Flatten(unittest.TestCase):
+	def setUp(self):
+		self.aggregate_list = lambda kwargs, data : kwargs['lst'].append(data)
+		'''
+      1
+    /   \
+   2     3
+ /  \   /  \
+4    5 6    7
+		'''
+		l = [1,2,3,4,5,6,7]
+		self.bt = TreeMaker.fromList(l)
+
+		'''
+      1
+    /   
+   2
+ /   \
+4     5
+		'''
+		self.bt2 = TreeMaker.fromList([1, 2, None, 4, 5])
+
+		'''
+        1
+      /   \
+     2     3
+   /         \
+  4           5
+ /             \
+6               7
+		'''
+		self.bt3 = TreeMaker.fromList([1, 2, 3, 4, None, None, 5, 6, None, None, None, None, None, None, 7])
+
+
+	def test_inorder_flatten_to_dll(self):
+		bt = self.bt
+		(head, tail) = FlattenBinaryTree.binTreeToDLL_in(bt)
+		assert head.value == 4
+		assert tail.value == 7
+
+		forward = []
+		tmp = head
+		while tmp:
+			forward.append(tmp.value)
+			tmp = tmp.right
+
+		reverse = []
+		tmp = tail
+		while tmp:
+			reverse.append(tmp.value)
+			tmp = tmp.left
+
+		assert forward == [4,2,5,1,6,3,7]
+		assert reverse == forward[::-1]
+
+
+		bt = self.bt2
+		(head, tail) = FlattenBinaryTree.binTreeToDLL_in(bt)
+		assert head.value == 4
+		assert tail.value == 1
+
+		forward = []
+		tmp = head
+		while tmp:
+			forward.append(tmp.value)
+			tmp = tmp.right
+
+		reverse = []
+		tmp = tail
+		while tmp:
+			reverse.append(tmp.value)
+			tmp = tmp.left
+
+		assert forward == [4,2,5,1]
+		assert reverse == forward[::-1]
+
+
+		bt = self.bt3
+		(head, tail) = FlattenBinaryTree.binTreeToDLL_in(bt)
+		assert head.value == 6
+		assert tail.value == 7
+
+		forward = []
+		tmp = head
+		while tmp:
+			forward.append(tmp.value)
+			tmp = tmp.right
+
+		reverse = []
+		tmp = tail
+		while tmp:
+			reverse.append(tmp.value)
+			tmp = tmp.left
+
+		assert forward == [6,4,2,1,3,5,7]
+		assert reverse == forward[::-1]
+
+
+if __name__ == '__main__':
+	unittest.main()
+
