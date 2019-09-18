7efc17f8eb407b863307bd0e5c1e259d9e40f957
Niranjan Nagaraju <vinithepooh@gmail.com>
2019-09-18 13:16:53 +0530

    Binary Tree: Flatten tree to DLL / Consolidate repeated code in testcases to a single function

--- python/data_structures/trees/binary_tree/testcases/test_flatten.py
+++ python/data_structures/trees/binary_tree/testcases/test_flatten.py
@@ -38,132 +38,67 @@ class Test_Flatten(unittest.TestCase):
 		'''
 		self.bt3 = TreeMaker.fromList([1, 2, 3, 4, None, None, 5, 6, None, None, None, None, None, None, 7])
 
+		'''
+    1
+   / \
+  2   5
+ / \   \
+3   4   6
+		'''
+		self.bt4 = TreeMaker.fromList([1,2,5,3,4,None,6])
 
 	def test_inorder_flatten_to_dll(self):
-		bt = self.bt
-		(head, tail) = FlattenBinaryTree.binTreeToDLL_in(bt)
-		assert head.value == 4
-		assert tail.value == 7
+		def _test(bt, inorder_sequence):
+			(head, tail) = FlattenBinaryTree.binTreeToDLL_in(bt)
+			assert head.value == inorder_sequence[0]
+			assert tail.value == inorder_sequence[-1]
 
-		forward = []
-		tmp = head
-		while tmp:
-			forward.append(tmp.value)
-			tmp = tmp.right
+			forward = []
+			tmp = head
+			while tmp:
+				forward.append(tmp.value)
+				tmp = tmp.right
 
-		reverse = []
-		tmp = tail
-		while tmp:
-			reverse.append(tmp.value)
-			tmp = tmp.left
+			reverse = []
+			tmp = tail
+			while tmp:
+				reverse.append(tmp.value)
+				tmp = tmp.left
 
-		assert forward == [4,2,5,1,6,3,7]
-		assert reverse == forward[::-1]
+			assert forward == inorder_sequence
+			assert reverse == forward[::-1]
 
-
-		bt = self.bt2
-		(head, tail) = FlattenBinaryTree.binTreeToDLL_in(bt)
-		assert head.value == 4
-		assert tail.value == 1
-
-		forward = []
-		tmp = head
-		while tmp:
-			forward.append(tmp.value)
-			tmp = tmp.right
-
-		reverse = []
-		tmp = tail
-		while tmp:
-			reverse.append(tmp.value)
-			tmp = tmp.left
-
-		assert forward == [4,2,5,1]
-		assert reverse == forward[::-1]
+		_test(self.bt, [4,2,5,1,6,3,7])
+		_test(self.bt2, [4,2,5,1])
+		_test(self.bt3, [6,4,2,1,3,5,7])
+		_test(self.bt4, [3,2,4,1,5,6])
 
 
-		bt = self.bt3
-		(head, tail) = FlattenBinaryTree.binTreeToDLL_in(bt)
-		assert head.value == 6
-		assert tail.value == 7
+	def test_preorder_flatten_to_dll(self):
+		def _test(bt, preorder_sequence):
+			(head, tail) = FlattenBinaryTree.binTreeToDLL_pre(bt)
+			assert head.value == preorder_sequence[0]
+			assert tail.value == preorder_sequence[-1]
 
-		forward = []
-		tmp = head
-		while tmp:
-			forward.append(tmp.value)
-			tmp = tmp.right
+			forward = []
+			tmp = head
+			while tmp:
+				forward.append(tmp.value)
+				tmp = tmp.right
 
-		reverse = []
-		tmp = tail
-		while tmp:
-			reverse.append(tmp.value)
-			tmp = tmp.left
+			reverse = []
+			tmp = tail
+			while tmp:
+				reverse.append(tmp.value)
+				tmp = tmp.left
 
-		assert forward == [6,4,2,1,3,5,7]
-		assert reverse == forward[::-1]
+			assert forward == preorder_sequence
+			assert reverse == forward[::-1]
 
-
-	def test_postorder_flatten_to_dll(self):
-		bt = self.bt
-		(head, tail) = FlattenBinaryTree.binTreeToDLL_pre(bt)
-		assert head.value == 1
-		assert tail.value == 7
-
-		forward = []
-		tmp = head
-		while tmp:
-			forward.append(tmp.value)
-			tmp = tmp.right
-
-		reverse = []
-		tmp = tail
-		while tmp:
-			reverse.append(tmp.value)
-			tmp = tmp.left
-
-		assert forward == [1,2,4,5,3,6,7]
-		assert reverse == forward[::-1]
-
-		bt = self.bt2
-		(head, tail) = FlattenBinaryTree.binTreeToDLL_pre(bt)
-		assert head.value == 1
-		assert tail.value == 5
-
-		forward = []
-		tmp = head
-		while tmp:
-			forward.append(tmp.value)
-			tmp = tmp.right
-
-		reverse = []
-		tmp = tail
-		while tmp:
-			reverse.append(tmp.value)
-			tmp = tmp.left
-
-		assert forward == [1,2,4,5]
-		assert reverse == forward[::-1]
-
-
-		bt = self.bt3
-		(head, tail) = FlattenBinaryTree.binTreeToDLL_pre(bt)
-		assert head.value == 1
-		assert tail.value == 7
-
-		forward = []
-		tmp = head
-		while tmp:
-			forward.append(tmp.value)
-			tmp = tmp.right
-
-		reverse = []
-		tmp = tail
-		while tmp:
-			reverse.append(tmp.value)
-			tmp = tmp.left
-
-		assert forward == [1,2,4,6,3,5,7]
-		assert reverse == forward[::-1]
+		_test(self.bt, [1,2,4,5,3,6,7])
+		_test(self.bt2, [1,2,4,5])
+		_test(self.bt3, [1,2,4,6,3,5,7])
+		_test(self.bt4, [1,2,3,4,5,6])
 
 
 if __name__ == '__main__':
