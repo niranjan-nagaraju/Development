6c22f70be51e1dc8dece99d643fcdc3429d03173
Niranjan Nagaraju <vinithepooh@gmail.com>
2019-09-25 20:22:00 +0530

    Leetcode/Trapping rain water: Stack-based O(n) solution

--- python/leetcode/trapping_rain_water/trapping_rain_water.py
+++ python/leetcode/trapping_rain_water/trapping_rain_water.py
@@ -35,20 +35,20 @@ Solution outline:
 	4. To keep the stack ordered by bar height (and that the values are always decreasing from the bottom of the stack to the top), we 'reduce' the stack anytime a higher value than stacktop is found.
 	   Reduce by popping all bars (< current bar height, aka the grooves) from the top until a left anchor >= current bar is found,
 	     At this point, we have a left anchor, right anchor, grooves and some blocks part of these grooves that cannot trap water.
-		 Calculate water trapped by discounting the blocks, and using a height = min(left_anchor, right_anchor)
-	   If the stack becomes empty, then we restart water storage starting from the current bar, using it as the left anchor
+		 Once water is trapped in these grooves, they essentially become blocks of height min(left_anchor, right_anchor)
+		 So we can always calculate water trapped between a left and right anchor as min(left, right) - groove
+		 e.g.,
+		 [3,2,1,2,4]
+		 [2,1,2] -> becomes == [2,2,2] after trapping 1 unit of water
+		 now [3,2,2,2,4] ==> [3,2,4], left:3, right: 4, groove = 2 => they can trap (min(3,4)-2)*3 == (3-2)*3 == 3 units of water
 	5. Reduction rules:
 		Until stacktop, st < current bar, curr
+		   right anchor = current bar
 		   pop stacktop -- These are the grooves,
-		   We keep track of the blocks in between by adding bar popped.
-		   At this point, stacktop >= popped bar, Add popped bar height to blocks
-		   These blocks count the number of blocks in the current (un-reduced) window that cannot store water.
-		   if the two bars (stacktop, current bar) are separated by distance 'd',
-		   then max water that can be potentially stored between st and curr = min(st,curr)*d
-		   Actual water trapped = potential water storage - blocks
+		   At this point, stacktop >= popped bar => stacktop = left anchor
+		   if the two bars (left, right) are separated by distance 'd', stacktop => groove
+		   Actual water trapped = (min(left,right)-groove)*d
 		   Total water trapped += actual water trapped
-		   Add actual water to 'blocks' - This is because once water has been trapped in a groove,
-		   we can discount that groove in further calculations.
 			  e.g.
 			   [3,2,1,2]
 			   [2,1,2] can be reduced to 1 units of water, and effectively be considered [2,2,2] after
@@ -56,9 +56,14 @@ Solution outline:
 			   then, [3,2,1,2,3] => [3,2,2,2,3] == 3 units of water trapped between [3..3]
 			   effectively bringing the total water trapped between [3,2,1,2,3] to 4
 			   --
+			        _
+			    _  | |
+			   | | | |
+			   | |_| |
+			   |_|_|_|
 			   e.g. [3,1,4]
-			   between the 3 and 4, blocks = 1, potential =  2*3 == 6units
-			   actual water trapped = 5 units
+			   between the 3 and 4, groove = 1
+			   actual water trapped = (min(3,4)-1)*1 == (3-1)*1 == 2 units
 			   --
 			   e.g.
 			   [3,2,1,4]
@@ -69,48 +74,34 @@ Solution outline:
 			   |_|_|_|_|
 			   stack: [1,2,3], at 4
 			   > pop 1
-			   blocks += 1 = 1
 			   stack: [2,3]
-			   top: 2
-			   left: 2, right: 4, blocks in between = 1
-			   actual water trapped between 2 and 4 is (2)*(1) - 1 = 1 units
-			   Total water trapped += 1 == 1
-			   blocks += 1 == 2
+			   groove = 1, right = 4, left = stacktop = 2
+			   water trapped: (min(4,2)-1)*1 == 1*1 == 1 units
+			   stack: [2,3]
 			   > pop 2
-			   blocks += 2 == 4
 			   stack: [3]
-			   top: 3
-			   left: 3, right: 4, blocks in between = 4
-			   actual water trapped between 3 and 4 is (3)*(2) - 4 = 2 units
-			   Total water trapped += 2 == 3
-			   blocks += 2 == 6
-			   > pop 3
-			   blocks += 3 == 9
-			   stack: []
-			   top: NA
-			   left: ?, right: 4, blocks in between = 9
-			   stack: empty, push (4)
+			   groove = 2, right = 4, left = 3
+			   water trapped: (min(4,3)-2)*2 == (3-2)*2 == 2 units
+			   > pop: 3
+			   groove = 3, right = 4, left = NA
 	    Once all bars < current are popped,
 		    push current bar
-			if the stack was empty before pushing current bar, reset blocks and water trapped in current reduction of the stack.
 
 		   e.g.,
-		   blocks = 0
 		   bar: [2,1,3]
 		   At some stage, stack: [1,2], current: 3
 		   1 < 3, 
 		   pop: 1
-		   blocks += 1 == 1
 		   stack: [2]
-		   potential water storage between curr,stacktop is (2-0-1)*min(2,3) == 2
-		   water = potential - blocks = 2-1 = 1
-		   blocks += 1 == 2
+		   left = 2, right = 3, groove = 1
+		   water trapped = (min(2,3)-1) * 1 == 1 units
+		   stack: [2]
 		   pop: 2
 		   stack: []
-		   stack is now empty, reset blocks = 0, total water = 1, curr water trapped = 0
-		   push 3
-		   stack: [3] <-- Everything until 3 has been reduced, start a new 'window' anchored at bar 3
-		
+		   left = NA, right = 3, groove = 2
+		   push (3)
+		   stack: [3]
+
 
 Sample run 1:
 =============
@@ -123,128 +114,82 @@ Sample run 1:
 
 	   Stack: []
 	   Total water = 0
-	   current window blocks = 0
 
 	   i: 0
 	   bar: 3
 	   stack is empty
 	   push 3,0
 	   stack: [(3,0)]
-		_ 
-	   | |
-	   | |
-	   |_|
 
 	   i: 1
 	   bar: 2
 	   stacktop < 2? NO
 	   push 2,1
 	   stack: [(2,1), (3,0)]
-		_   
-	   | |_ 
-	   | | |
-	   |_|_|
 
 	   i: 2
 	   bar: 1
 	   stacktop < 1? NO
 	   stack: [(1,2), (2,1), (3,0)]
-		_   
-	   | |_ 
-	   | | |_
-	   |_|_|_|
 
 	   i: 3
 	   bar: 0
 	   stacktop < 0? NO
 	   stack: [(0,3), (1,2), (2,1), (3,0)]
-		_   
-	   | |_ 
-	   | | |_
-	   |_|_|_|_
 
 	   i: 4
 	   bar: 1
 	   stacktop == 0 < 1
 	   pop: 0,3
-	   blocks += 0 = 0
+	   groove = 0
 	   stack: [(1,2), (2,1), (3,0)]
-	   top: 1,2
-	   water trapped = min(1,1)*(4-2-1) - blocks == 1
+	   top: 1,2 => left = 1
+	   right = 1
+	   water trapped = [min(1,1)-0]*(4-2-1)  == 1
 	   Total water trapped += 1 = 1
-	   blocks += 1 = 1
-		_   
-	   | |_ 
-	   | | |_
-	   |_|_|_|_
-	   --
-	   stacktop == 1 < 1
-	   pop 1,2
-	   blocks += 1 = 2
-	   stack: [(2,1), (3,0)]
-	   top: 2,1
-	   water trapped = min(2,1)*(4-1-1) - blocks == 1*2-2 = 0
-	   Total water trapped += 0 = 1
-	   blocks += 0 == 2
-		_   
-	   | |_ 
-	   | | |_
-	   |_|_|_|_
-	   --
-	   stacktop == 2 < 1? NO
-	   push 1,4
-	   stack: [(1,4), (2,1), (3,0)]
                   _ 
 		_        | | 
 	   | |_      | |
 	   | | |_   _| |
 	   |_|_|_|W|_|_|
+	   --
+	   stacktop == 1 < 1? NO
+	   push (1,4)
+	   stack: [(1,4), (1,2), (2,1), (3,0)]
 
-	   i: 4
-	   bar: 5
-	   stacktop = 1 < 4
+	   i: 5
+	   bar: 4
+	   stacktop == 1 < 4
 	   pop: 1,4
-	   blocks += 1 = 3
+	   groove = 1
+	   stack: [(1,2), (2,1), (3,0)]
+	   top: 1,2 => left: 1
+	   water trapped: min(4,1)-1 * d == 0
+	   --
+	   stacktop == 1 < 4
+	   pop: 1,2
+	   groove = 1
 	   stack: [(2,1), (3,0)]
-	   top: 2,1
-	   water trapped = min(4,2)*(5-1-1) - blocks == 2*3 - 3 == 3
-	   blocks += 3 == 6
-	   Total water trapped += 3 = 4
+	   top: 2,1 => left: 2
+	   water trapped: min(4,2)-1 * (5-1-1) == 1*3 == 3 units
                   _ 
 		_        | | 
 	   | |_      | |
 	   | | |W W W| |
 	   |_|_|_|W|_|_|
-
 	   --
 	   stacktop = 2 < 4
 	   pop: 2,1
-	   blocks += 2 = 8
+	   groove = 2
 	   stack: [(3,0)]
-	   top: 3,0
-	   water trapped = min(3,4)*(5-0-1) - blocks = 3*4 - 8 == 4
-	   blocks += 4 == 12
-	   Total water trapped += 4 == 8
+	   top: 3,0 => left: 3
+	   water trapped: min(4,3)-2 * (5-0-1) == 1*4 == 4 units
                   _ 
-		_        | | 
+   		_        | | 
 	   | |W W W W| |
 	   | | |W W W| |
 	   |_|_|_|W|_|_|
 
-	   --
-	   stacktop = 3 < 4
-	   pop: 3,0
-	   top: []
-	   stack: []
-	   blocks = 0
-	   push 4,5
-                   _
-		 _        | |
-		| |W W W W| |
-		| | |W W W| |
-		|_|_|_|W|_|_|
-
-		total water: 8
 
 
 Sample run 2:
@@ -253,7 +198,6 @@ Sample run 2:
 	Index:  0 1 2 3 4 5 6 7 8 9 10 11
 	stack: []
 	Total water: 0
-	blocks: 0
 
 	i: 0
 	bar: 0
@@ -265,10 +209,8 @@ Sample run 2:
 	bar: 1
 	stacktop < 1
 	pop: 0,0
-	blocks += 0 = 0
 	stack: []
 	top: NA
-	blocks = 0 = 0
 	push (1,1)
 	Stack: [(1,1)]
 
@@ -283,10 +225,9 @@ Sample run 2:
 	stacktop = 0 < 2
 	pop: 0,2
 	stack: [(1,1)]
-	blocks += 0 = 0
-	top: 1,1
-	water trapped = min(1,2) * (3-1-1) - blocks == 1*1 - 0 = 1
-	blocks += 1 = 1
+	top: 1,1 => left = 1
+	groove = 0
+	water trapped = [min(1,2)-0] * (3-1-1) == 1*1 = 1
 	Total water trapped += 1 = 1
                _
        _      | |_   _
@@ -298,7 +239,6 @@ Sample run 2:
 	pop: 1,1
 	top: empty
 	stack: []
-	reset, blocks = 0
 	push (2,3)
 	stack: [(2,3)]
 
@@ -318,11 +258,11 @@ Sample run 2:
 	bar: 1
 	stacktop = 0 < 1
 	pop: 0,5
-	blocks +=0 = 0
 	stack: [(1,4), (2,3)]
-	top: 1,4
-	water trapped = min(1,1) * (6-4-1) - blocks == 1*1 - 0 = 1
-	blocks += 1 = 1
+	top: 1,4 => left: 1
+	groove = 0
+	right = 1
+	water trapped = [min(1,1)-0] * (6-4-1)  == 1*1 - 0 = 1
 	Total water trapped += 1 = 2
                _
        _      | |_   _
@@ -334,25 +274,21 @@ Sample run 2:
 	push 1,6
 	stack: [(1,6), (1,4), (2,3)]
 
-
 	i: 7
 	bar: 3
 	stacktop = 1 < 3
 	pop 1,6
-	blocks += 1 = 2
 	stack: [(1,4), (2,3)]
 	top: 1,4
-	water trapped = min(1,3) * (7-4-1) - blocks == 1*2 - 2 = 0
+	left: 1, right = 3, groove = 1
+	water trapped = min(1,1)-1 * (7-4-1) == 0*3 = 0
 	Total water trapped += 0 = 2
-	blocks += 0 = 2
 	--
 	stacktop = 1 < 3
-	pop 1,4
-	blocks += 1 = 3
+	pop 1,4 => groove: 1
 	stack: [(2,3)]
-	top: 2,3
-	water trapped = min(2,3) * (7-3-1) - blocks == 2*3 - 3 = 3
-	blocks += 3 = 6
+	top: 2,3 => left: 2
+	water trapped = [min(2,3) - 1]* (7-3-1) == 1*3 = 3
 	Total water trapped += 3 = 5
                _
        _      | |_   _
@@ -364,7 +300,6 @@ Sample run 2:
 	pop 2,3
 	top: NA
 	stack: []
-	blocks = 0
 	push (3,7)
 	stack: [(3,7)]
 
@@ -385,10 +320,10 @@ Sample run 2:
 	stacktop = 1 < 2
 	pop 1,9
 	stack: [(2,8), (3,7)]
-	top: 2,8
-	blocks += 1 == 1
-	water trapped = min(2,2) * (10-8-1) - blocks == 2*1 - 1 = 1
-	blocks += 1 = 2
+	top: 2,8 => left = 2
+	groove = 1
+	right = 2
+	water trapped = [min(2,2)-1] * (10-8-1) == 1*1 = 1
 	Total water trapped += 1 = 6
                _
        _      | |_   _
@@ -418,38 +353,17 @@ class Solution(object):
 		"""
 		total_water_trapped = 0
 
-		blocks = 0
 		stack = []
 		for i in xrange(len(height)):
-			print 'Current:', height[i], i
 			while stack and stack[0][0] < height[i]:
-				print stack
-				curr_bar,_ = stack.pop(0)
-
-				print 'Popped:', curr_bar
-				
+				groove,_ = stack.pop(0)
 				if not stack:
 					break
 
-				blocks += curr_bar
 				top, idx = stack[0]
-				water_trapped = min(top, height[i]) * (i-idx-1) - blocks
-
-				print 'Top:', top, idx
-				print 'Blocks after pop:', blocks
-				print 'Water trapped between', (height[i], i), 'and', (top, idx), 'is:', water_trapped
+				water_trapped = (min(top, height[i]) - groove) * (i-idx-1)
 
 				total_water_trapped += water_trapped
-				blocks += water_trapped
-				print 'Blocks after water:', blocks
-
-
-			# stack is empty
-			if not stack:
-				# reset current window
-				# as height[i] will be the new left anchor
-				# and everything to the left of height[i] has been reduced
-				blocks = 0
 
 			stack.insert(0,(height[i], i))
 
@@ -463,4 +377,5 @@ if __name__ == '__main__':
 	assert s.trap([0,1,0,2,1,0,1,3,2,1,2,1]) == 6
 	assert s.trap([2,1,2,6,9,7,5,5,7]) == 5
 	assert s.trap([4,2,3]) == 1
-	assert s.trap([4,2,0,3,2,4,3,4]) == 10 # FIXME: actual returned value is -6
+	assert s.trap([3,2,1,0,1,5]) == 8
+	assert s.trap([4,2,0,3,2,4,3,4]) == 10
