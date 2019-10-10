b0edbad67a056cd56282679117caa9ea0318ef3f
Niranjan Nagaraju <vinithepooh@gmail.com>
2019-10-10 20:57:12 +0530

    Binary Tree: Clone a binary tree/iterative version

--- python/data_structures/trees/binary_tree/clone_tree.py
+++ python/data_structures/trees/binary_tree/clone_tree.py
@@ -47,3 +47,50 @@ class CloneTree(object):
 
 		return BinaryTree(root=_clone(bintree.root))
 
+
+	'''
+	Iterative version of the clone tree method
+
+	Uses iterative post-order traversal
+	'''
+	@staticmethod
+	def clone_i(bintree):
+		s = Stack()
+		# contains cloned node references, a', for each node a
+		# doubles as visited[] map => if cloned_nodes[a] is not empty => a is visited
+		cloned_nodes = {}
+
+		if not bintree or not bintree.root:
+			return None
+
+		# Helper function to check if a node is already 'done'/processed
+		# a node is considered 'done'
+		#   if its value is None
+		#   or
+		#   if it has already been cloned.
+		isDone = lambda node, cloned_node: True if (node is None or cloned_node is not None) else False
+
+		s.push(bintree.root)
+		while s:
+			node = s.top()
+			left_clone = cloned_nodes.get(node.left)
+			right_clone = cloned_nodes.get(node.right)
+			# Both left and right subtrees are done
+			# pop 'node' off the stack, create a clone, and map the clone to the original
+			# Link cloned node's left and right children
+			if isDone(node.left, left_clone) and isDone(node.right, right_clone):
+				s.pop()
+				node_ = Node(node.value)
+				node_.left = left_clone
+				node_.right = right_clone
+				cloned_nodes[node] = node_
+			else:
+				# push both left and right children to the stack
+				# NOTE: This actually processes reverse post-order (right-left-root)
+				#       unless we push right, followed by left onto the stack
+				#       but order shouldnt really matter
+				s.push(node.left) if node.left else None
+				s.push(node.right) if node.right else None
+
+		return BinaryTree(root=cloned_nodes[bintree.root])
+
--- python/data_structures/trees/binary_tree/testcases/test_clone_tree.py
+++ python/data_structures/trees/binary_tree/testcases/test_clone_tree.py
@@ -38,16 +38,100 @@ class Test_CloneTree(unittest.TestCase):
 		self.bt3 = TreeMaker.fromList([1, 2, 3, 4, None, None, 5, 6, None, None, None, None, None, None, 7])
 
 
-	def test_clone_tree(self):
-		bt = self.bt
-		inorder = []
-		bt.inorder_traversal(self.aggregate_list, lst=inorder)
 
-		bt_copy = CloneTree.clone_r(self.bt)
-		inorder_copy = []
-		bt_copy.inorder_traversal(self.aggregate_list, lst=inorder_copy)
-		assert inorder == inorder_copy == [4,2,5,1,6,3,7]
+
+	# Test recursive version of the clone method
+	def test_clone_tree_recursive(self):
+		bt = self.bt
+		bt_copy = CloneTree.clone_r(bt)
+		inorder, preorder, postorder = [], [], []
+		inorder_c, preorder_c, postorder_c = [], [], []
+		bt.inorder_traversal(self.aggregate_list, lst=inorder)
+		bt_copy.inorder_traversal(self.aggregate_list, lst=inorder_c)
+		bt.postorder_traversal(self.aggregate_list, lst=postorder)
+		bt_copy.postorder_traversal(self.aggregate_list, lst=postorder_c)
+		bt.preorder_traversal(self.aggregate_list, lst=preorder)
+		bt_copy.preorder_traversal(self.aggregate_list, lst=preorder_c)
+		assert inorder == inorder_c == [4,2,5,1,6,3,7]
+		assert postorder == postorder_c == [4,5,2,6,7,3,1]
+		assert preorder == preorder_c == [1,2,4,5,3,6,7]
+
+		bt = self.bt2
+		bt_copy = CloneTree.clone_r(bt)
+		inorder, preorder, postorder = [], [], []
+		inorder_c, preorder_c, postorder_c = [], [], []
+		bt.inorder_traversal(self.aggregate_list, lst=inorder)
+		bt_copy.inorder_traversal(self.aggregate_list, lst=inorder_c)
+		bt.postorder_traversal(self.aggregate_list, lst=postorder)
+		bt_copy.postorder_traversal(self.aggregate_list, lst=postorder_c)
+		bt.preorder_traversal(self.aggregate_list, lst=preorder)
+		bt_copy.preorder_traversal(self.aggregate_list, lst=preorder_c)
+		assert inorder == inorder_c == [4,2,5,1]
+		assert postorder == postorder_c == [4,5,2,1]
+		assert preorder == preorder_c == [1,2,4,5]
+
+		bt = self.bt3
+		bt_copy = CloneTree.clone_r(bt)
+		inorder, preorder, postorder = [], [], []
+		inorder_c, preorder_c, postorder_c = [], [], []
+		bt.inorder_traversal(self.aggregate_list, lst=inorder)
+		bt_copy.inorder_traversal(self.aggregate_list, lst=inorder_c)
+		bt.postorder_traversal(self.aggregate_list, lst=postorder)
+		bt_copy.postorder_traversal(self.aggregate_list, lst=postorder_c)
+		bt.preorder_traversal(self.aggregate_list, lst=preorder)
+		bt_copy.preorder_traversal(self.aggregate_list, lst=preorder_c)
+		assert inorder == inorder_c == [6,4,2,1,3,5,7]
+		assert postorder == postorder_c == [6,4,2,7,5,3,1]
+		assert preorder == preorder_c == [1,2,4,6,3,5,7]
+
+
+
+	# Test iterative version of the clone method
+	def test_clone_tree_iterative(self):
+		bt = self.bt
+		bt_copy = CloneTree.clone_i(bt)
+		inorder, preorder, postorder = [], [], []
+		inorder_c, preorder_c, postorder_c = [], [], []
+		bt.inorder_traversal(self.aggregate_list, lst=inorder)
+		bt_copy.inorder_traversal(self.aggregate_list, lst=inorder_c)
+		bt.postorder_traversal(self.aggregate_list, lst=postorder)
+		bt_copy.postorder_traversal(self.aggregate_list, lst=postorder_c)
+		bt.preorder_traversal(self.aggregate_list, lst=preorder)
+		bt_copy.preorder_traversal(self.aggregate_list, lst=preorder_c)
+		assert inorder == inorder_c == [4,2,5,1,6,3,7]
+		assert postorder == postorder_c == [4,5,2,6,7,3,1]
+		assert preorder == preorder_c == [1,2,4,5,3,6,7]
+
+		bt = self.bt2
+		bt_copy = CloneTree.clone_i(bt)
+		inorder, preorder, postorder = [], [], []
+		inorder_c, preorder_c, postorder_c = [], [], []
+		bt.inorder_traversal(self.aggregate_list, lst=inorder)
+		bt_copy.inorder_traversal(self.aggregate_list, lst=inorder_c)
+		bt.postorder_traversal(self.aggregate_list, lst=postorder)
+		bt_copy.postorder_traversal(self.aggregate_list, lst=postorder_c)
+		bt.preorder_traversal(self.aggregate_list, lst=preorder)
+		bt_copy.preorder_traversal(self.aggregate_list, lst=preorder_c)
+		assert inorder == inorder_c == [4,2,5,1]
+		assert postorder == postorder_c == [4,5,2,1]
+		assert preorder == preorder_c == [1,2,4,5]
+
+		bt = self.bt3
+		bt_copy = CloneTree.clone_i(bt)
+		inorder, preorder, postorder = [], [], []
+		inorder_c, preorder_c, postorder_c = [], [], []
+		bt.inorder_traversal(self.aggregate_list, lst=inorder)
+		bt_copy.inorder_traversal(self.aggregate_list, lst=inorder_c)
+		bt.postorder_traversal(self.aggregate_list, lst=postorder)
+		bt_copy.postorder_traversal(self.aggregate_list, lst=postorder_c)
+		bt.preorder_traversal(self.aggregate_list, lst=preorder)
+		bt_copy.preorder_traversal(self.aggregate_list, lst=preorder_c)
+		assert inorder == inorder_c == [6,4,2,1,3,5,7]
+		assert postorder == postorder_c == [6,4,2,7,5,3,1]
+		assert preorder == preorder_c == [1,2,4,6,3,5,7]
+
 
 
 if __name__ == '__main__':
 	unittest.main()
+
