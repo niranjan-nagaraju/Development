ad49f40164e1b221723891ba5397812be821d08a
Niranjan Nagaraju <vinithepooh@gmail.com>
2019-10-31 22:12:52 +0530

    Abacus sort: L-R implementation sorts temp array n descending order and have to be copied into the original array in reverse

--- python/algorithms/sorts/abacus_sort.py
+++ python/algorithms/sorts/abacus_sort.py
@@ -4,98 +4,171 @@ Abacus sort / Bead sort/ Gravity sort (efficient O(n) space, O(mn) time) impleme
   m: max - min (in the list)
 '''
 
+
 '''
+Version 1: Scan input array L-R so the temp array is sorted in ascending order
 Sample run:
-2 5 1 4
+a[]: 2 5 1 4
 
 min: 1, max: 5
-a: 1 1 1 1
 
-i: 1 to 5
- j: 3 to 0
+Initialize temp[] with min
+temp:
+      +---------------+
+      | 1 | 1 | 1 | 1 |
+      +---------------+
+
+Iterate
+i: min(1) to max-1(4)
+ j: 3 to 0 (R-L)
 
 
 i: 1
+                    j,k
+      +---------------+
+      | 1 | 1 | 1 | 1 |
+      +---------------+
+
   j: 3
-   4 > 1
-   a: 1 1 1 2
- 
+   a[j] = 4 > 1
+               j,k  
+      +---------------+
+      | 1 | 1 | 1 | 2 |
+      +---------------+
+
   j: 2
-   1 > 1? NO
+   a[j] > 1? NO
+            j   k  
+      +---------------+
+      | 1 | 1 | 1 | 2 |
+      +---------------+
 
   j: 1
-   5 > 1
-   a: 1 1 2 2
+   a[j] = 5 > 1
+        j   k  
+      +---------------+
+      | 1 | 1 | 2 | 2 |
+      +---------------+
 
   j: 0
-   2 > 1
-   a: 1 2 2 2
+   a[j] = 2 > 1
+   temp: 1 2 2 2
+        k    
+      +---------------+
+      | 1 | 2 | 2 | 2 |
+      +---------------+
 
 
 i: 2
+                    j,k
+      +---------------+
+      | 1 | 2 | 2 | 2 |
+      +---------------+
+
  j: 3
-  4 > 2
-  a: 1 2 2 3
+  a[j] = 4 > 2
+               j,k
+      +---------------+
+      | 1 | 2 | 2 | 3 |
+      +---------------+
 
  j: 2
-  1 > 2? NO
+  a[j] = 1 > 2? NO
+            j   k
+      +---------------+
+      | 1 | 2 | 2 | 3 |
+      +---------------+
+
  
- j: 2
-  5 > 2
-  a: 1 2 3 3
+ j: 1
+  a[j] = 5 > 2
+        j   k
+      +---------------+
+      | 1 | 2 | 3 | 3 |
+      +---------------+
 
  j: 0
-  2 > 2? NO 
+  a[j] = 2 > 2? NO 
+            k   
+      +---------------+
+      | 1 | 2 | 3 | 3 |
+      +---------------+
+
 
 
 i: 3
+                    j,k
+      +---------------+
+      | 1 | 2 | 3 | 3 |
+      +---------------+
+
   j: 3
-   4 > 3
-   a: 1 2 3 4
+   a[j] = 4 > 3
+               j,k
+      +---------------+
+      | 1 | 2 | 3 | 4 |
+      +---------------+
 
   j: 2
-   1 > 3? NO
+   a[j] = 1 > 3? NO
+            j   k
+      +---------------+
+      | 1 | 2 | 3 | 4 |
+      +---------------+
 
   j: 1
-   5 > 3
-   a: 1 2 4 4
+   a[j] = 5 > 3
+        j   k   
+      +---------------+
+      | 1 | 2 | 4 | 4 |
+      +---------------+
 
   j: 0
-   2 > 3? NO
+   a[j] = 2 > 3? NO
+            k   
+      +---------------+
+      | 1 | 2 | 4 | 4 |
+      +---------------+
 
 
 i: 4
+                   j,k   
+      +---------------+
+      | 1 | 2 | 4 | 4 |
+      +---------------+
+
   j: 3
-   4 > 4? NO
+   a[j] = 4 > 4? NO
+                j   k   
+      +---------------+
+      | 1 | 2 | 4 | 4 |
+      +---------------+
+
 
   j: 2
-   1 > 4? NO
+   a[j] = 1 > 4? NO
+            j       k   
+      +---------------+
+      | 1 | 2 | 4 | 4 |
+      +---------------+
 
   j: 1
-   5 > 4
-   a: 1 2 4 5
+   a[j] = 5 > 4
+        j       k   
+      +---------------+
+      | 1 | 2 | 4 | 5 |
+      +---------------+
 
   j: 0
    2 > 4? NO
+                k   
+      +---------------+
+      | 1 | 2 | 4 | 5 |
+      +---------------+
 
-
-i: 5
-  j: 3
-    4 > 5? NO
-
-  j: 2
-   1 > 5? NO
-
-  j: 1
-   5 > 5? NO
-
-  j: 0
-   2 > 5? NO
-
-
-a: 1 2 4 5
+temp: 1 2 4 5
+copy into a: [1,2,4,5]
 '''
-
 def abacus_sort(a):
 	minimum, maximum = a[0], a[0]
 	n = len(a)
@@ -123,12 +196,129 @@ def abacus_sort(a):
 
 
 
-if __name__ == '__main__':
+
+'''
+Version 2:
+	Scans input array L-R and the resulting temporary sorted array will be in decreasing order.
+
+
+Sample run:
+2 5 1 4
+
+min: 1, max: 5
+temp: 1 1 1 1
+
+i: 1 to max-1(4)
+ j: 0 to 3
+
+
+i: 1
+  j: 0
+   2 > 1
+   temp: 2 1 1 1
+ 
+  j: 1
+   5 > 1
+   temp: 2 2 1 1
+
+  j: 2
+   1 > 1? NO
+
+  j: 3
+   4 > 1
+   temp: 2 2 2 1
+
+i: 2
+  j: 0
+   2 > 2? NO
+ 
+  j: 1
+   5 > 2
+   temp: 3 2 2 1
+
+  j: 2
+   1 > 2? NO
+
+  j: 3
+   4 > 2
+   temp: 3 3 2 1
+
+
+i: 3
+  j: 0
+   2 > 3? NO
+ 
+  j: 1
+   5 > 3
+   temp: 4 3 2 1
+
+  j: 2
+   1 > 3? NO
+
+  j: 3
+   4 > 3
+   temp: 4 4 2 1
+
+
+i: 4
+  j: 0
+   2 > 4? NO
+ 
+  j: 1
+   5 > 4
+   temp: 5 4 2 1
+
+  j: 2
+   1 > 4? NO
+
+  j: 3
+   4 > 4? NO
+'''
+def abacus_sort_2(a):
+	minimum, maximum = a[0], a[0]
+	n = len(a)
+
+	for x in a[1:]:
+		# a[i] cannot be both maximum and minimum at the same time
+		if x < minimum:
+			minimum = x
+		elif x > maximum:
+			maximum = x
+
+	# Initialize a temporary array filled with minimum
+	temp = [minimum] * n
+	for i in xrange(minimum, maximum):
+		k = 0
+		for j in xrange(n):
+			if a[j] > i:
+				temp[k] += 1
+				k += 1
+
+	# Copy temp array back into original array
+	# replacing the array into sorted order
+	# temp array is reverse sorted, so copy backwards for ascending order
+	for i in xrange(n):
+		a[i] = temp[n-i-1]
+
+
+
+def test_abacus_sort(sortfn):
 	a = [2,5,1,4]
-	abacus_sort(a)
+	sortfn(a)
 	assert a == [1,2,4,5]
 
 	b = [1, 2, 4, 2, 5, 3, 2, 3, 4]
-	abacus_sort(b)
+	sortfn(b)
 	assert b == [1,2,2,2,3,3,4,4,5]
+
+	# negative numbers
+	c = [3, -1 , 2, -2, 4, 0, 1, 5, 3]
+	sortfn(c)
+	assert c == [-2, -1, 0, 1, 2, 3, 3, 4, 5]
+
+
+if __name__ == '__main__':
+	test_abacus_sort(abacus_sort)
+	test_abacus_sort(abacus_sort_2)
+
 
