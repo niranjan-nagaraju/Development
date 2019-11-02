70fff40cadf97c06c84d644ce2f2e9132e6e652b
Niranjan Nagaraju <vinithepooh@gmail.com>
2019-11-02 15:55:11 +0530

    Vertical histogram: One upside down version and another that starts lower and grows upwards like in a regular histogram

--- python/algorithms/arrays/historgram/vertical_histogram.py
+++ python/algorithms/arrays/historgram/vertical_histogram.py
@@ -1,5 +1,7 @@
+import sys
+
 '''
-Print a vertical histogram using * for a given set of numbers
+Print an upside-down vertical histogram using * for a given set of numbers
 
 e.g.,
 
@@ -13,8 +15,7 @@ Histogram:
       *
       *
 '''
-import sys
-def print_histogram(a):
+def print_histogram_upside_down(a):
 	maximum = a[0]
 	for x in a[1:]:
 		if x > maximum:
@@ -25,10 +26,38 @@ def print_histogram(a):
 			sys.stdout.write('* ' if a[j] > i else '  ')
 		print
 
+'''
+Print a vertical histogram using * for a given set of numbers
+
+e.g.,
+
+a = [2, 6, 1, 4, 3]
+
+Histogram:
+
+      *
+      *
+      *   *
+      *   * *
+	* *   * *
+    * * * * *
+'''
+def print_histogram(a):
+	maximum = a[0]
+	for x in a[1:]:
+		if x > maximum:
+			maximum = x
+
+	for i in xrange(maximum-1, -1, -1):
+		for j in xrange(len(a)):
+			sys.stdout.write('* ' if a[j] > i else '  ')
+		print
+
+
 
 
 if __name__ == '__main__':
-	print_histogram([2,6,1,4,3])
+	print_histogram_upside_down([2,6,1,4,3])
 	'''
 	* * * * * 
 	* *   * * 
@@ -36,4 +65,13 @@ if __name__ == '__main__':
 	  *   *   
 	  *       
 	  *       
+	'''
+	print_histogram([2,6,1,4,3])
+	'''
+      *       
+      *       
+      *   *   
+      *   * * 
+    * *   * * 
+    * * * * * 
 	'''
