308b935da8dfa36518a03656c0a0783cdf999d05
Niranjan Nagaraju <vinithepooh@gmail.com>
2019-12-27 20:27:08 +0530

    Disjoint set: Union find - use parent instead of representative

--- python/data_structures/disjoint_set/node.py
+++ python/data_structures/disjoint_set/node.py
@@ -3,8 +3,8 @@ Node in a disjoint-set data structure
 Contains
   key: data/value of the set item
   rank: number of children under this Node
-        (relevant if the node is a representative of the set)
-  representative: representative for the set this node belongs to,
+        (relevant if the node is a parent of the set)
+  parent: parent for the set this node belongs to,
         None if this node is the representative for the set.
 '''
 
@@ -12,7 +12,7 @@ class Node(object):
 	def __init__(self, key):
 		self.key = key
 		self.rank = 0
-		self.representative = None
+		self.parent = None
 
 	
 	def __str__(self):
@@ -20,7 +20,7 @@ class Node(object):
 
 
 	def __repr__(self):
-		return '{key: ' + str(self.key) + ', rank: ' + str(self.rank) + ', rep: ' + str(self.representative) + '}'
+		return '{key: ' + str(self.key) + ', rank: ' + str(self.rank) + ', parent: ' + str(self.parent) + '}'
 
 
 
--- python/data_structures/disjoint_set/union_find.py
+++ python/data_structures/disjoint_set/union_find.py
@@ -2,7 +2,44 @@
 Disjoint-sets/Union-Find
   Use union by ranking, and path-compression optimizations to reduce the levels of lookup
 '''
+from node import Node
+from collections import defaultdict
 
+'''
+Disjoint-set using union by rank and path-compression
+'''
 class DisjointSet(object):
 	def __init__(self):
-		mapping = {}
+		# maps data to their respective nodes in the disjoint set
+		# for quick lookup
+		self.nodes_map = defaultdict(lambda: None)
+
+	
+	'''
+	Make a singleton set containing 'value'
+	'''
+	def make_set(self, value):
+		if self.nodes_map[value] is None:
+			return
+		self.nodes_map[value] = Node(value)
+
+
+	'''
+	Merge two sets containing lvalue, and rvalue respectively
+	into one
+	'''
+	def union(self, lvalue, rvalue):
+		pass
+
+
+	'''
+	Find set containing a specified value
+	'''
+	def find(self, value):
+		def find_(node):
+			if node.parent == None:
+				return node
+			node.parent = find_(node.parent)
+			return node.parent
+
+
