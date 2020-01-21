9d068e83d294c1234247929dd820a4e580ee5f36
Niranjan Nagaraju <vinithepooh@gmail.com>
2020-01-21 20:42:33 +0530

    Binary search: support optional lower and upper bound ranges to search within a sub-array

--- python/algorithms/search/binary_search/binary_search.py
+++ python/algorithms/search/binary_search/binary_search.py
@@ -2,48 +2,51 @@
 Binary search on a sorted list
 '''
 
-
 # A Recursive binary search implementation
 # Return the index where the item 'x' was found on the list
-def binary_search_r(lst, x):
+def binary_search_r(lst, key, l=0, h=None):
 	# Find x in lst[l:h]
-	def binary_search_helper(lst, x, l, h):
+	def binary_search_helper(l, h):
 		# List's low and high indices have criss-crossed
 		# => x could not be found
 		if l > h:
 			return -1
 
 		mid = (l+h)/2
-		if lst[mid] == x:
+		if lst[mid] == key:
 			return mid
-		elif x > lst[mid]:
+		elif key > lst[mid]:
 			# search in second half of the sub-array
-			return binary_search_helper(lst, x, mid+1, h)
+			return binary_search_helper(mid+1, h)
 		else:
 			# search in first half of the sub-array
-			return binary_search_helper(lst, x, l, mid-1)
+			return binary_search_helper(l, mid-1)
 
 
 	# Call the helper
-	return binary_search_helper(lst, x, 0, len(lst)-1)
+	if h is None:
+		h = len(lst)-1
+	return binary_search_helper(l, h)
 
 
 
 # Iterative implementation of the binary search
-def binary_search_i(lst, x):
-	l, h = 0, len(lst)-1
+def binary_search_i(lst, key, l=0, h=None):
+	if h is None:
+		h = len(lst)-1
+
 	while l <= h:
 		mid = (l+h)/2
-		if lst[mid] == x:
+		if lst[mid] == key:
 			return mid
-		elif x > lst[mid]:
+		elif key > lst[mid]:
 			# search in second half of the sub-array
 			l = mid+1
 		else:
 			# search in first half of the sub-array
 			h = mid-1
 
-	# Couldn't find 'x'
+	# Couldn't find 'key'
 	return -1
 
 
@@ -74,6 +77,14 @@ def test_binary_search(search_f):
 	assert(search_f([2,4,6, 8], 5) == -1)
 	assert(search_f([2,4,6, 8], 7) == -1)
 
+	# search 5 in [1..10] but within the subarray [6..10]
+	assert(search_f(range(1, 11), 5, 5, 9) == -1)
+
+	# search 5 in [1..10] but within the subarray [3..10]
+	assert(search_f(range(1, 11), 5, 2, 9) == 4)
+
+	# [10, 9, 4, 5, 6, 7, 8, 9, 10, 5], search '6' within indices 2-8 -> [4..10] which are sorted.
+	assert(search_f([10, 9] + range(4, 11) + [5], 6, 2, 8) == 4)
 
 
 
