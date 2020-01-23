5a4165ec233bdae29be479d8523b7d6b4577b129
Niranjan Nagaraju <vinithepooh@gmail.com>
2020-01-23 14:48:56 +0530

    Leetcode Find minimum/pivot in rotated sorted array I and II: Add testcase that covers both left and right subarrays while looking for pivot

--- python/leetcode/find_minimum_in_rotated_sorted_array/find_minimum_in_rotated_sorted_array.py
+++ python/leetcode/find_minimum_in_rotated_sorted_array/find_minimum_in_rotated_sorted_array.py
@@ -61,6 +61,7 @@ class Solution(object):
 
 if __name__ == '__main__':
 	s = Solution()
+	assert s.findMin([4,5,6,7,1,2]) == 1
 	assert s.findMin([4,5,6,7,0,1,2]) == 0
 	assert s.findMin([3,4,5,1,2]) == 1
 	assert s.findMin([11,1,2,3,4,5,6,7,8,9,10]) == 1
--- python/leetcode/find_minimum_in_rotated_sorted_array_ii/find_minimum_in_rotated_sorted_array_ii.py
+++ python/leetcode/find_minimum_in_rotated_sorted_array_ii/find_minimum_in_rotated_sorted_array_ii.py
@@ -83,6 +83,7 @@ if __name__ == '__main__':
 
 	# Solution should work even if there aren't any duplicates
 	# or if the sorted array wasn't rotated in the first place
+	assert s.findMin([4,5,6,7,1,2]) == 1
 	assert s.findMin([4,5,6,7,0,1,2]) == 0
 	assert s.findMin([3,4,5,1,2]) == 1
 	assert s.findMin([11,1,2,3,4,5,6,7,8,9,10]) == 1
