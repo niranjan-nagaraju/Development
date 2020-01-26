ba51aae6ac6d38b50a24e82fe79f8ec44bd814ae
Niranjan Nagaraju <vinithepooh@gmail.com>
2020-01-26 13:38:00 +0530

    Interviewbit/Nearest smaller element: stack-based O(n) solution

---
+++ python/interviewbit/nearest_smaller_element/nearest_smaller_element.py
@@ -0,0 +1,99 @@
+'''
+https://www.interviewbit.com/problems/nearest-smaller-element/
+
+Nearest Smaller Element
+
+Given an array, find the nearest smaller element G[i] for every element A[i] in the array such that the element has an index smaller than i.
+
+More formally,
+    G[i] for an element A[i] = an element A[j] such that 
+    j is maximum possible AND 
+    j < i AND
+    A[j] < A[i]
+
+Elements for which no smaller element exist, consider next smaller element as -1.
+
+Input Format
+
+The only argument given is integer array A.
+Output Format
+
+Return the integar array G such that G[i] contains nearest smaller number than A[i].If no such element occurs G[i] should be -1.
+For Example
+
+Input 1:
+    A = [4, 5, 2, 10, 8]
+Output 1:
+    G = [-1, 4, -1, 2, 2]
+Explaination 1:
+    index 1: No element less than 4 in left of 4, G[1] = -1
+    index 2: A[1] is only element less than A[2], G[2] = A[1]
+    index 3: No element less than 2 in left of 2, G[3] = -1
+    index 4: A[3] is nearest element which is less than A[4], G[4] = A[3]
+    index 4: A[3] is nearest element which is less than A[5], G[5] = A[3]
+    
+Input 2:
+    A = [3, 2, 1]
+Output 2:
+    [-1, -1, -1]
+Explaination 2:
+    index 1: No element less than 3 in left of 3, G[1] = -1
+    index 2: No element less than 2 in left of 2, G[2] = -1
+    index 3: No element less than 1 in left of 1, G[3] = -1
+'''
+
+'''
+Solution Outline:
+	Use a stack.
+	Scan elements left to right,
+	  for every element, x, remove everything from the stack that is >= x (as x will be a better minimum)
+	  If there's a stack top, then mark it as x's nearest-smallest-element, otherwise x doesnt have an NSE.
+'''
+class Stack:
+	def __init__(self):
+		self.items = []
+
+	def __len__(self):
+		return len(self.items)
+
+	def push(self, x):
+		self.items.append(x)
+
+	def pop(self):
+		return self.items.pop()
+
+	def top(self):
+		return self.items[-1] if self.items else -1
+
+
+class Solution:
+	# @param A : list of integers
+	# @return a list of integers
+	def prevSmaller(self, A):
+		s = Stack()
+		nse = []
+
+		for x in A:
+			while s and s.top() >= x:
+				s.pop()
+
+			# Mark x's nse as stack top if it exists, else -1
+			nse.append(s.top())
+
+			s.push(x)
+
+		return nse
+
+
+if __name__ == '__main__':
+	s = Solution()
+	assert s.prevSmaller([1,2,3,4,5]) == [-1,1,2,3,4]
+	assert s.prevSmaller([1,1,1,1,1]) == [-1]*5
+	assert s.prevSmaller([4,5,2,10,8]) == [-1,4,-1,2,2]
+	assert s.prevSmaller([3,2,1]) == [-1]*3
+	assert s.prevSmaller([1,4,3,2,5]) == [-1, 1, 1, 1, 2]
+
+
+			
+
+
