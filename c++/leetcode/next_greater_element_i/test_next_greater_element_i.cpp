f6867573d829fb2996936688549afff50715d9e2
Niranjan Nagaraju <vinithepooh@gmail.com>
2020-02-18 11:48:01 +0530

    Leetcode/Next Greater Element I: C++ solution - Use static initializers for vectors in testcases

--- c++/leetcode/next_greater_element_i/test_next_greater_element_i.cpp
+++ c++/leetcode/next_greater_element_i/test_next_greater_element_i.cpp
@@ -4,34 +4,31 @@ using namespace std;
 #include "next_greater_element_i.hpp"
 
 void
-test_next_greater_element(int *n1, int n1sz, int *n2, int n2sz, int *expected_nge, int nge_sz)
+//test_next_greater_element(int *n1, int n1sz, int *n2, int n2sz, int *expected_nge, int nge_sz)
+test_next_greater_element(vector<int> &a, vector<int> &b, vector<int> &expected_nge)
 {
 	Solution s;
 	int i;
 
-	vector<int> a(n1, n1+n1sz);
-	vector<int> b(n2, n2+n2sz);
-	vector<int> e_nge(expected_nge, expected_nge+nge_sz);
-
 	vector<int>nge = s.nextGreaterElement(a, b);
 
-	assert (nge == e_nge);
+	assert (nge == expected_nge);
 }
 
 int
 main(void)
 {
 	Solution s;
-	int a[] = {1,2,3,4};
-	int a_[] = {2,4};
-	int expected_a[] = {3, -1};
+	vector<int> a{1,2,3,4};
+	vector<int> a_{2,4};
+	vector<int> expected_a{3, -1};
 
-	test_next_greater_element(a_, 2, a, 4, expected_a, 2);
+	test_next_greater_element(a_, a, expected_a);
 
-	int b[] = {1,3,4,2};
-	int b_[] = {4,1,2};
-	int expected_b[] = {-1, 3, -1};
-	test_next_greater_element(b_, 3, b, 4, expected_b, 3);
+	vector<int> b{1,3,4,2};
+	vector<int> b_{4,1,2};
+	vector<int> expected_b{-1, 3, -1};
+	test_next_greater_element(b_, b, expected_b);
 
 	cout<<"Tests complete\n";
 }
