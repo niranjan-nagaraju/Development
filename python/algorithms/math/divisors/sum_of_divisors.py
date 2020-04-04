ea241d7d7a800fb8d3b5b9893d07e4d64c742e0d
Niranjan Nagaraju <vinithepooh@gmail.com>
2020-04-04 16:23:50 +0530

    Sum of divisors of a number

--- python/algorithms/math/divisors/sum_of_divisors.py
+++ python/algorithms/math/divisors/sum_of_divisors.py
@@ -1,5 +1,10 @@
-#!/usr/bin/python
+'''
+n = p1 ** a1 * p2 ** a2 ...
+then
+sum_of_divisors_including_n = (1+ p1 + p1**2 + .. + p1**a1) * (1+ p2 + .. p2**a2) * ...
+'''
 
+from algorithms.math.divisors.divisors import divisors
 def sum_of_divisors(n):
 	prod = 1
 	k = 2
@@ -16,10 +21,8 @@ def sum_of_divisors(n):
 
 	return prod
 
-print sum_of_divisors(24)
 
-'''
-n = p1 ** a1 * p2 ** a2 ...
-then
-sum_of_divisors_including_n = (1+ p1 + p1**2 + .. + p1**a1) * (1+ p2 + .. p2**a2) * ...
-'''
+if __name__ == '__main__':
+	assert (sum_of_divisors(24) == sum(divisors(24)) == 60)
+	assert (sum_of_divisors(18) == sum(divisors(18)) == 39)
+
