3cb9563fb4cd7e0f466702a535b2889ca3eb9169
Niranjan Nagaraju <vinithepooh@gmail.com>
2020-05-09 18:45:54 +0530

    Interviewbit/Reverse the string: Compress multiple spaces, reverse individual words and reverse the whole string to get a reversed order of words

--- python/interviewbit/strings/reverse_the_string/reverse_the_string.py
+++ python/interviewbit/strings/reverse_the_string/reverse_the_string.py
@@ -1,3 +1,34 @@
+'''
+https://www.interviewbit.com/problems/reverse-the-string/
+
+Reverse the String
+
+Given a string A.
+
+Return the string A after reversing the string word by word.
+
+NOTE:
+A sequence of non-space characters constitutes a word.
+Your reversed string should not contain leading or trailing spaces, even if it is present in the input string.
+If there are multiple spaces between words, reduce them to a single space in the reversed string.
+
+Input Format
+The only argument given is string A.
+
+Output Format
+Return the string A after reversing the string word by word.
+
+For Example
+Input 1:
+    A = "the sky is blue"
+Output 1:
+    "blue is sky the"
+
+Input 2:
+    A = "this is ib"
+Output 2:
+    "ib is this"
+'''
 class Solution:
 	# @param A : string
 	# @return a strings
