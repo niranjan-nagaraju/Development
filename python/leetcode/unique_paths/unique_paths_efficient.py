ec38412f9812832f4e72f9d231aa55f621810a70
Niranjan Nagaraju <vinithepooh@gmail.com>
2020-08-04 15:34:35 +0530

    Leetcode/Unique Paths I and II: Count number of unique paths in a grid from top-left to bottom-right, I: grid has no obstacles, II: grid with obstacles

---
+++ python/leetcode/unique_paths/unique_paths_efficient.py
@@ -0,0 +1,87 @@
+'''
+https://leetcode.com/problems/unique-paths/
+
+62. Unique Paths
+
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+
+|---+---+---+---+---+---+---|
+| S |   |   |   |   |   |   |
+|---+---+---+---+---+---+---|
+|   |   |   |   |   |   |   |
+|---+---+---+---+---+---+---|
+|   |   |   |   |   |   | E |
+|---+---+---+---+---+---+---|
+
+Above is a 7 x 3 grid. How many possible unique paths are there?
+
+Example 1:
+Input: m = 3, n = 2
+Output: 3
+Explanation:
+From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Right -> Down
+2. Right -> Down -> Right
+3. Down -> Right -> Right
+
+Example 2:
+Input: m = 7, n = 3
+Output: 28
+ 
+
+Constraints:
+1 <= m, n <= 100
+It's guaranteed that the answer will be less than or equal to 2 * 10 ^ 9.
+'''
+
+
+'''
+Solution Outline: O(n) DP
+	In an AxB grid, S=(0,0), E=(A-1, B-1)
+	Let f(x,y) be the number of ways to reach cell (x,y) from S
+	f(0,1) = Number of ways to reach (0,1) from S == 1 (R)
+	f(1,0) = Number of ways to reach (1,0) from S == 1 (D)
+
+	f(x,0) = Number of ways to reach (x,0) from S == 1 (DD...D)
+	f(0,y) = Number of ways to reach (0,y) from S == 1 (RR...R)
+
+	For any other cell (i,j), Paths to enter it are from its top and left-side cells.
+	  Therefore the number of ways to reach cell (i,j) == f(i,j) = f(i-1, j) + f(i, j-1)
+
+	Fill the table bottom-up, i: 0 to A, j: 0 to B, return f(A-1, B-1)
+
+Sample run for the grid above:
+|----+----+----+----+----+----+----|
+|  0 |  1 |  1 |  1 |  1 |  1 |  1 |
+|----+----+----+----+----+----+----|
+|  1 |  2 |  3 |  4 |  5 |  6 |  7 |
+|----+----+----+----+----+----+----|
+|  1 |  3 |  6 | 10 | 15 | 21 | 28 |
+|----+----+----+----+----+----+----|
+
+'''
+class Solution(object):
+	def uniquePaths(self, m, n):
+		"""
+		:type m: int
+		:type n: int
+		:rtype: int
+		"""
+		DP = [[1 for _ in xrange(n)] for _ in xrange(2)]
+		for i in xrange(1, m):
+			for j in xrange(1, n):
+				DP[i&1][j] = DP[i&1][j-1] + DP[(i-1)&1][j]
+
+		return DP[(m-1)&1][-1]
+
+
+if __name__ == '__main__':
+	s = Solution()
+	assert s.uniquePaths(2, 2) == 2
+	assert s.uniquePaths(3, 7) == 28
+	assert s.uniquePaths(3, 3) == 6
+
---
+++ python/leetcode/unique_paths_ii/unique_paths_ii.py
@@ -0,0 +1,191 @@
+'''
+https://leetcode.com/problems/unique-paths-ii/
+
+63. Unique Paths II
+
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+Now consider if some obstacles are added to the grids. How many unique paths would there be?
+An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+
+Note: m and n will be at most 100.
+
+Example 1:
+Input:
+[
+  [0,0,0],
+  [0,1,0],
+  [0,0,0]
+]
+Output: 2
+Explanation:
+There is one obstacle in the middle of the 3x3 grid above.
+There are two ways to reach the bottom-right corner:
+1. Right -> Right -> Down -> Down
+2. Down -> Down -> Right -> Right
+'''
+
+
+'''
+Solution Outline:
+    0. Allowed directions are R, D
+    1. Consider moving to cell x,y from 0,0
+        If there were no obstacles, it would be (num_paths_to(x-1,y) + num_paths_to(x,y-1))
+        with num_paths_to(x,0) == 1, (only direction allowed is down)
+        and num_paths_to(0,y) == 1 (only direction allowed is right) {for any 0<=x<m,0<=y<n}
+    2. With obstacles,
+        if x,0 is an obstacle,
+            then the column looks like (x=2 in the example)
+            [[0
+             [0
+             [1
+             [0
+             [0 0 . . .
+        num_paths_to(0,0) = 1
+        num_paths_to(1,0) = 1
+        num_paths_to(2,0) = 0 (blockade)
+        num_paths_to(3,0) = 0 (can' get past blockade moving only D)
+        num_paths_to(4,0) = 0
+
+        Similarly, if (0,y) is an obstacle,
+            then the first row looks like (y=1 in the example)
+            [[0 1 0 0 0 0]
+        num_paths_to(0,0) = 1
+        num_paths_to(0,1) = 0 (blockade)
+        num_paths_to(0,y) = 0 (for all y > 1) (can't get past blockade moving only R)
+
+        For any random(x,y),
+          if x,y is an obstacle, then num_paths_to(x,y) = 0
+          otherwise,
+          num_paths_to(x,y) = sum(num_paths_to(x-1,y), num_paths_to(x,y-1))
+
+Sample run 1:
+A= [
+      [0,0,0],
+      [0,1,0],
+      [0,0,0]
+    ]
+
+DP: [
+      [0,0,0],
+      [0,0,0],
+      [0,0,0]
+    ]
+
+Fill DP row 0,
+DP: [
+      [1,1,1],
+      [0,0,0],
+      [0,0,0]
+    ]
+Fill DP col 0,
+DP: [
+      [1,1,1],
+      [1,0,0],
+      [1,0,0]
+    ]
+
+(x,y): (1,1) is a blockade
+DP: [
+      [1,1,1],
+      [1,0,0],
+      [1,0,0]
+    ]
+
+(x,y): (1,2) == sum(left, up) == sum(DP[1,1], DP[0,2]) == 1
+DP: [
+      [1,1,1],
+      [1,0,1],
+      [1,0,0]
+    ]
+
+(x,y): (2,1) == sum(left,up) == sum(DP[2,0], DP[1,1]) == 1
+DP: [
+      [1,1,1],
+      [1,0,1],
+      [1,1,0]
+    ]
+
+(x,y): (2,2) == sum(left,up) == sum(DP[2,1], DP[1,2]) == 2
+DP: [
+      [1,1,1],
+      [1,0,1],
+      [1,1,2]
+    ]
+'''
+class Solution(object):
+	def uniquePathsWithObstacles(self, obstacleGrid):
+		"""
+		:type obstacleGrid: List[List[int]]
+		:rtype: int
+		"""
+
+		if not obstacleGrid:
+			return 0
+
+		m = len(obstacleGrid)
+		n = len(obstacleGrid[0])
+
+		# End cell is blocked
+		if obstacleGrid[-1][-1] == 1:
+			return 0
+
+		DP = [[0 for _ in xrange(n)] for _ in xrange(m)]
+
+		# first row
+		for j in xrange(n):
+			if obstacleGrid[0][j] == 1:
+				break
+			DP[0][j] = 1
+
+		# first column
+		for i in xrange(m):
+			if obstacleGrid[i][0] == 1:
+				break
+			DP[i][0] = 1
+
+		for i in xrange(1, m):
+			for j in xrange(1, n):
+				if obstacleGrid[i][j] == 0:
+					DP[i][j] = DP[i-1][j] + DP[i][j-1]
+				# if A[i][j] is an obstacle, DP[i][j] remains 0
+
+		return DP[-1][-1]
+
+
+if __name__ == '__main__':
+	s = Solution()
+	assert s.uniquePathsWithObstacles(\
+		[
+		  [0,0,0],
+		  [0,1,0],
+		  [0,0,0]
+		]) == 2
+
+	assert s.uniquePathsWithObstacles(\
+		[
+		  [0,0,0],
+		  [0,1,0],
+		  [0,0,1]
+		]) == 0
+
+	assert s.uniquePathsWithObstacles(\
+		[
+		  [0,0,1,0],
+		  [0,1,0,0],
+		  [0,0,0,0],
+		  [1,0,0,0]
+		]) == 3
+
+	assert s.uniquePathsWithObstacles(\
+		[
+		  [0,0,1,0],
+		  [0,1,0,0],
+		  [0,0,0,0],
+		  [0,0,0,0],
+		  [1,0,0,0]
+		]) == 9
+
+
