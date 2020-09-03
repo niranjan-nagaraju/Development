da81a8f9883edc3b154448e3b94c9c3dd0a25478
Niranjan Nagaraju <vinithepooh@gmail.com>
2020-09-03 23:06:01 +0530

    Scala/Factorial: Tail-recursion optimized version

---
+++ scala/math/factorial/factorial.scala
@@ -0,0 +1,14 @@
+
+/* tail recursive factorial */
+
+def factorial(n: Int, acc: Int = 1): Int = {
+  if (n == 0)
+    acc
+  else
+    //tail recursion -> optimize
+    factorial(n-1, acc*n)
+}
+
+assert(factorial(0) == 1)
+assert(factorial(5) == 120)
+assert(factorial(10) == 3628800)
