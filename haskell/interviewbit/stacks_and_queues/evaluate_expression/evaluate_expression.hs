3a6218b83d6ad61f7240b0e1ba79af7892b3f08b
Niranjan Nagaraju <vinithepooh@gmail.com>
2020-09-28 21:50:49 +0530

    Interviewbit/Evaluate expression: Evaluate an expression in reverse polish(postfix) notation, Haskell solution - add problem description/link

--- haskell/interviewbit/stacks_and_queues/evaluate_expression/evaluate_expression.hs
+++ haskell/interviewbit/stacks_and_queues/evaluate_expression/evaluate_expression.hs
@@ -1,3 +1,60 @@
+{-
+https://www.interviewbit.com/problems/evaluate-expression/
+
+Evaluate Expression
+
+
+Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+
+Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+
+
+Input Format
+The only argument given is character array A.
+
+Output Format
+Return the value of arithmetic expression formed using reverse Polish Notation.
+
+For Example
+Input 1:
+    A =   ["2", "1", "+", "3", "*"]
+Output 1:
+    9
+Explaination 1:
+    starting from backside:
+    *: ( )*( )
+    3: ()*(3)
+    +: ( () + () )*(3)
+    1: ( () + (1) )*(3)
+    2: ( (2) + (1) )*(3)
+    ((2)+(1))*(3) = 9
+    
+Input 2:
+    A = ["4", "13", "5", "/", "+"]
+Output 2:
+    6
+Explaination 2:
+    +: ()+()
+    /: ()+(() / ())
+    5: ()+(() / (5))
+    1: ()+((13) / (5))
+    4: (4)+((13) / (5))
+    (4)+((13) / (5)) = 6
+-}
+
+
+{-
+Solution Outline:
+	1. Use a stack to reduce expressions as operators are encountered.
+	2. The stack stores only operands, and 
+		2.1 on encountering an operator,
+		2.2 pop right followed by the left operand
+		2.3 Apply operator on the operands, and push the reduced result onto the stack.
+	3. For each operand, push it onto the stack.
+-}
+
+
+
 import Control.Exception (assert)
 
 
