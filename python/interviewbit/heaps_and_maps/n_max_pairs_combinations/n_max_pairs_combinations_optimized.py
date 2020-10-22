ba5b48c955676cfc465385bc43702f9237efc994
Niranjan Nagaraju <vinithepooh@gmail.com>
2020-10-22 21:27:13 +0530

    Interviewbit/n-max-pair-sum combinations: Add sample run using a max-heap and a set to de-dup

--- python/interviewbit/heaps_and_maps/n_max_pairs_combinations/n_max_pairs_combinations_optimized.py
+++ python/interviewbit/heaps_and_maps/n_max_pairs_combinations/n_max_pairs_combinations_optimized.py
@@ -36,6 +36,41 @@ Solution:
 		4.1 entry <- sum, i, j
 		4.2 Add sum to the list, 
 			Add A[i]+B[j+1] and A[i+1]+B[j] to the max-heap unless they are already part of the max-heap.
+
+Sample run:
+	A[]={1,4,2,3}
+	B[]={2,5,1,6}
+
+	heap: []
+	result: []
+
+	A': [4,3,2,1]
+	B': [6,5,2,1]
+
+	heap: [(10,0,0)]
+	
+	extract: 10,0,0
+	result: [10]
+	A[1]+B[0], A[0]+B[1] => 9, 9
+	heap: [(9,1,0), (9,0,1)]
+
+	extract: 9, 1, 0
+	result: [10, 9]
+	A[2]+B[0], A[1]+B[1] = 4, 8
+	heap: [(9,0,1), (8,1,1), (4,2,0)]
+
+	extract: 9, 0, 1
+	result: [10, 9, 9]
+	A[1]+B[1], A[1]+B[2] = 8, 5 {(8,1,1) already exists in the heap}
+	heap: [(8,1,1), (4,2,0), (5,1,2)]
+
+	extract: 8, 1, 1
+	result: [10, 9, 9, 8]
+	A[1]+B[2], A[2]+B[1] = 5, 7 {(5,1,2) already exists in the heap}
+	heap: [(5,2,0), (5,1,2), (7,2,1)]
+
+	Extracted 4 maximum sums,
+	return [10, 9, 9, 8]
 '''
 
 from maxheap import MaxHeap
