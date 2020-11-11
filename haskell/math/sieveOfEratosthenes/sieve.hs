56ea7d7ec8fe453f905833d7a1e3ccbff7d15f71
Niranjan Nagaraju <vinithepooh@gmail.com>
2020-11-11 22:25:52 +0530

    Separate out module and testcases into different files

--- haskell/math/sieveOfEratosthenes/sieve.hs
+++ haskell/math/sieveOfEratosthenes/sieve.hs
@@ -1,4 +1,10 @@
-import Control.Exception (assert)
+module Sieve (
+	sieve,
+	first_n_primes,
+	nth_prime,
+	primes_below,
+	primes_between
+) where
 
 {-
  - Build a sieve of eratosthenes recursively
@@ -36,23 +42,3 @@ primes_between m n =
 	dropWhile (<m) $ takeWhile (<=n) sieve
 
 
-main = do
-	putStr $ assert ( (take 10) sieve == [2,3,5,7,11,13,17,19,23,29] ) ""
-
-	putStr $ assert ( (first_n_primes 10) == [2,3,5,7,11,13,17,19,23,29] ) ""
-	putStr $ assert ( (first_n_primes 1) == [2] ) ""
-
-	putStr $ assert ( (nth_prime 1) == 2 ) ""
-	putStr $ assert ( (nth_prime 10) == 29 ) ""
-	putStr $ assert ( (nth_prime 100) == 541 ) ""
-
-	putStr $ assert ( (primes_below 100) ==
-		[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97] ) ""
-	putStr $ assert ( (primes_below 10) == [2,3,5,7] ) ""
-
-	putStr $ assert ( (primes_between 7 100) ==
-		[7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97] ) ""
-	putStr $ assert ( (primes_between 10 53) ==
-		[11,13,17,19,23,29,31,37,41,43,47,53] ) ""
-
-	putStrLn "Testcases complete!"
