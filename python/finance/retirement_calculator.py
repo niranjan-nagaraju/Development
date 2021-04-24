4f5bcbe5e70d466ed244482cda43495c22686ddd
Niranjan Nagaraju <vinithepooh@gmail.com>
2021-04-24 17:08:01 +0530

    Fix formatting, stop computation when corpus runs out

--- python/finance/retirement_calculator.py
+++ python/finance/retirement_calculator.py
@@ -1,22 +1,31 @@
 corpus = 300.0 # In lakhs, = 3 Cr
-inflation = 9.0
-returns = 9.0 
-withdrawal_rate = 2.0 # Increases at <Inflation>% yoy
+inflation_rate = 9.0
+returns = 4.0 
+withdrawal_rate = 2.0 # Increases at <inflation_rate>% yoy
 
 def calculate (n):
-	print "Years\tCorpus\tReturns\tWithdraw\tCorpus remaining" 
+	headline = ["Year", "Corpus(L)",
+				"Withdraw@{}% +{}%(L)".format(withdrawal_rate, inflation_rate),
+				"Post-withdraw(L)",
+				"Returns@{}%(L)".format(returns),
+				"Net-corpus"]
+	print "{: >5} {: >15} {: >15} {: >15} {: >15} {: >15}".format(*headline)
+
 	withdrawal_amt = withdrawal_rate * corpus / 100.0
 	corpus_remaining = corpus - withdrawal_amt
-	print "%d\t%0.2f\t%0.2f\t%0.2f\t%0.2f" %(0, 
-			corpus_remaining+withdrawal_amt, 0, withdrawal_amt, corpus_remaining)
+	returns_on_corpus = round((returns * corpus_remaining / 100.0), 2)
+	print "{: >5} {: >15} {: >15} {: >15} {: >15} {: > 20}".format(1,
+			corpus, withdrawal_amt, corpus_remaining, returns_on_corpus, corpus_remaining+returns_on_corpus)
 
-	for i in xrange(1, n):
-		withdrawal_amt = round ((100 + inflation) * withdrawal_amt / 100.0, 2)
+	for i in xrange(2, n+1):
+		withdrawal_amt = round ((100 + inflation_rate) * withdrawal_amt / 100.0, 2)
+
+		if withdrawal_amt > corpus_remaining:
+			print "{: >5} Not enough dough!".format(i)
+			return
 		returns_on_corpus = round((returns * corpus_remaining / 100.0), 2)
 		corpus_remaining = round((100 + returns) * corpus_remaining / 100.0  - withdrawal_amt, 2)
+		print "{: >5} {: >15} {: >15} {: >15} {: >15} {: >20}".format(i,
+				corpus_remaining+withdrawal_amt, withdrawal_amt, corpus_remaining, returns_on_corpus, corpus_remaining+returns_on_corpus)
 
-		print "%d\t%0.2f\t%0.2f\t%0.2f\t%0.2f" %(i, 
-				corpus_remaining+withdrawal_amt, returns_on_corpus, withdrawal_amt, corpus_remaining)
-
-
-calculate(50)
+calculate(100)
