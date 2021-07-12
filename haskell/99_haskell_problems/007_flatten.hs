2d6e64ecadf2897fba556ea895ed221855a3ad37
Niranjan Nagaraju <vinithepooh@gmail.com>
2021-07-12 13:26:00 +0530

    99 haskell problems - 7. flatten list: alternate implementation using fold

--- haskell/99_haskell_problems/007_flatten.hs
+++ haskell/99_haskell_problems/007_flatten.hs
@@ -14,20 +14,26 @@
  -  []
  -}
 
--- Solutions shamelessly lifted from the wiki page. :)
 
 import Control.Exception( assert )
 
 data NestedList a = Elem a | List [NestedList a] deriving Show
  
+-- Solution shamelessly lifted from the wiki page. :)
 flatten :: NestedList a -> [a]
 flatten (Elem x) = [x]
 flatten (List x) = concatMap flatten x -- concatMap applies "flatten" to every element in the list and concatenates
 
+-- using (x:xs) pattern match
 flatten' :: NestedList a -> [a]
 flatten' (Elem x) = [x]
 flatten' (List []) = []
 flatten' (List(x:xs)) = flatten' x ++ flatten' (List xs)
+
+-- using fold
+flatten'' :: NestedList a -> [a]
+flatten'' (Elem x) = [x]
+flatten'' (List nl) = foldr (\inner acc -> (flatten'' inner) ++ acc) [] nl
 
 main = do
 	putStr $ assert ( (flatten (List []::NestedList Int)) == [] ) ""
@@ -39,4 +45,9 @@ main = do
 	putStr $ assert ( (flatten' (Elem 5)) == [5] ) ""
 	putStr $ assert ( (flatten' (List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]])) == [1,2,3,4,5] ) ""
 	putStr $ assert ( (flatten' (List [List [List [Elem 1]]])) == [1] ) ""
+
+	putStr $ assert ( (flatten'' (List []::NestedList Int)) == [] ) ""
+	putStr $ assert ( (flatten'' (Elem 5)) == [5] ) ""
+	putStr $ assert ( (flatten'' (List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]])) == [1,2,3,4,5] ) ""
+	putStr $ assert ( (flatten'' (List [List [List [Elem 1]]])) == [1] ) ""
 
