e1edbe47c73523f528debf4e9dd2d29bf6994532
Niranjan Nagaraju <vinithepooh@gmail.com>
2021-07-17 14:24:53 +0530

    99 haskell problems - 8: Compress/de-dup consecutive repeated elements in a list - alternate implementation using fold

--- haskell/99_haskell_problems/008_compress.hs
+++ haskell/99_haskell_problems/008_compress.hs
@@ -6,5 +6,17 @@ compress [] = []
 compress (x:xs) = 
 	[x] ++ compress (dropWhile (==x) xs)
 
+
+-- alternate implementation using fold
+-- add current item to accumulator only if it doesn't match the accumulator's last item
+compress' list = foldl (\acc x -> if acc == [] || x /= (last acc) then acc++[x] else acc ) [] list
+
+
 main = do
-	putStr $ assert ( (compress ["a","a","a","a","b","c","c","a","a","d","e","e","e","e"]) == ["a", "b", "c", "a", "d", "e"] ) ""
+	putStr $ assert ( (compress "") == "" ) ""
+	putStr $ assert ( (compress "aaaabccaadeeee") == "abcade" ) ""
+	putStr $ assert ( (compress [1,1,1,2,2,3,3,3,3,4,3,4]) == [1,2,3,4,3,4] ) ""
+
+	putStr $ assert ( (compress' "") == "" ) ""
+	putStr $ assert ( (compress' "aaaabccaadeeee") == "abcade" ) ""
+	putStr $ assert ( (compress' [1,1,1,2,2,3,3,3,3,4,3,4]) == [1,2,3,4,3,4] ) ""
