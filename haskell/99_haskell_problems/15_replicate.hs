998a83e110b7033fed46862ba15e8aa2bd003a22
Niranjan Nagaraju <vinithepooh@gmail.com>
2021-07-25 14:55:58 +0530

    99 haskell problems - 15. replicate -- replicate elements of a list by specified times

--- haskell/99_haskell_problems/15_replicate.hs
+++ haskell/99_haskell_problems/15_replicate.hs
@@ -1,8 +1,47 @@
-{- Replicate the elements of a list a given number of times. -}
+{-
+ - Problem 15
+ - (**) Replicate the elements of a list a given number of times.
+ -
+ - Example:
+ -
+ - * (repli '(a b c) 3)
+ - (A A A B B B C C C)
+ - Example in Haskell:
+ -
+ - λ> repli "abc" 3
+ - "aaabbbccc"
+ -}
 
+import Control.Exception( assert )
+
+-- replicate a single item by the specified times.
+-- repliItem 1 3 -> [1,1,1]
+repliItem x times = map (\_ -> x) [1..times]
+
+
+repli :: (Show a) => [a] -> Int -> [a]
 repli [] _ = []
-repli (x:xs) times = 
-	(map (\y -> x) [1..times]) ++ (repli xs times)
+repli (x:xs) times = (repliItem x times) ++ (repli xs times)
+
+-- alternate implementation using foldl
+repli' :: (Show a) => [a] -> Int -> [a]
+repli' list times = foldl (\acc x -> acc ++ repliItem x times) [] list
+
+-- alternate implementation using foldr
+repli'' :: (Show a) => [a] -> Int -> [a]
+repli'' list times = foldr (\x acc -> repliItem x times ++ acc) [] list
+
+
 
 main = do
-	print $ repli "abc" 3
+	putStr $ assert ( (repli "abc" 3) == "aaabbbccc" ) ""
+	putStr $ assert ( (repli [1,2,3,4] 4) == [1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4] ) ""
+	putStr $ assert ( (repli "abcd" 1) == "abcd" ) ""
+
+	putStr $ assert ( (repli' "abc" 3) == "aaabbbccc" ) ""
+	putStr $ assert ( (repli' [1,2,3,4] 4) == [1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4] ) ""
+	putStr $ assert ( (repli' "abcd" 1) == "abcd" ) ""
+
+	putStr $ assert ( (repli'' "abc" 3) == "aaabbbccc" ) ""
+	putStr $ assert ( (repli'' [1,2,3,4] 4) == [1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4] ) ""
+	putStr $ assert ( (repli'' "abcd" 1) == "abcd" ) ""
