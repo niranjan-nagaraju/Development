10a2ba10dd9ee40e49ab4ff25424bb830b38eace
Niranjan Nagaraju <vinithepooh@gmail.com>
2021-07-31 20:57:03 +0530

    99 haskell problems - 16. Drop every nth element from a list, alternate implementations

--- haskell/99_haskell_problems/16_dropEveryN.hs
+++ haskell/99_haskell_problems/16_dropEveryN.hs
@@ -1,4 +1,19 @@
-{- Drop every N'th element from a list. -}
+{-
+ - Problem 16
+ -
+ - (**) Drop every N'th element from a list.
+ -
+ - Example:
+ -
+ - * (drop '(a b c d e f g h i k) 3)
+ - (A B D E G H K)
+ - Example in Haskell:
+ -
+ - λ> dropEvery "abcdefghik" 3
+ - "abdeghk"
+ -}
+
+import Control.Exception( assert )
 
 dropEvery list 0 = list
 dropEvery list n = 
@@ -6,10 +21,39 @@ dropEvery list n =
 	where
 	dropEvery' [] _ _ = []
 	dropEvery' (x:xs) n index = 
-		if index `mod` n == 0
-			then dropEvery' xs n (index+1) 
-			else x : dropEvery' xs n (index+1)  	
+		if index == n
+			then dropEvery' xs n 1 
+			else x : dropEvery' xs n (index+1)
 
+
+-- using zip to enumerate, and list comprehensions.
+dropEvery' list 0 = list
+dropEvery' list n = [ x | (idx,x) <- zip [1..] list, idx `mod` n /= 0 ]
+
+-- using `take` and `drop`
+dropEvery'' :: [a] -> Int -> [a]
+dropEvery'' [] _ = []
+dropEvery'' list 0 = list
+dropEvery'' list n = (take (n-1) list) ++ dropEvery'' (drop n list)  n
 
 main = do
-	print $ dropEvery "abcdefghik" 3
+	putStr $ assert ( (dropEvery "abcdefghijk" 0) == ['a'..'k'] ) ""
+	putStr $ assert ( (dropEvery' "abcdefghijk" 0) == ['a'..'k'] ) ""
+	putStr $ assert ( (dropEvery'' "abcdefghijk" 0) == ['a'..'k'] ) ""
+
+	putStr $ assert ( (dropEvery "abcdefghijk" 3) == "abdeghjk" ) ""
+	putStr $ assert ( (dropEvery' "abcdefghijk" 3) == "abdeghjk" ) ""
+	putStr $ assert ( (dropEvery'' "abcdefghijk" 3) == "abdeghjk" ) ""
+
+	putStr $ assert ( (dropEvery "abcdefghijk" 1) == "" ) ""
+	putStr $ assert ( (dropEvery' "abcdefghijk" 1) == "" ) ""
+	putStr $ assert ( (dropEvery'' "abcdefghijk" 1) == "" ) ""
+
+	putStr $ assert ( (dropEvery "abcdefghik" 3) == "abdeghk" ) ""
+	putStr $ assert ( (dropEvery' "abcdefghik" 3) == "abdeghk" ) ""
+	putStr $ assert ( (dropEvery'' "abcdefghik" 3) == "abdeghk" ) ""
+
+	putStr $ assert ( (dropEvery "abcdefghik" 2) == "acegi" ) ""
+	putStr $ assert ( (dropEvery' "abcdefghik" 2) == "acegi" ) ""
+	putStr $ assert ( (dropEvery'' "abcdefghik" 2) == "acegi" ) ""
+
