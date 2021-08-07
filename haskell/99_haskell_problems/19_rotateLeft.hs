f5b56f2f474d6b45e243bce330fae48f9f667373
Niranjan Nagaraju <vinithepooh@gmail.com>
2021-08-07 22:40:52 +0530

    99 haskell problems, 19. rotate: rotate left by <n>, alternate implementations

--- haskell/99_haskell_problems/19_rotateLeft.hs
+++ haskell/99_haskell_problems/19_rotateLeft.hs
@@ -1,4 +1,6 @@
-{- Rotate a list N places to the left.
+{- Problem 19
+ -
+ - Rotate a list N places to the left.
  - On -ve N, rotate (len-N) places to the left
  -
  - Trial Run:
@@ -9,6 +11,8 @@
  -   "ghabcdef"
  -}
 
+import Control.Exception( assert )
+
 rotate list n 
 	| (n == 0) = list
 	| (n > 0) = rotate' list n
@@ -18,7 +22,48 @@ rotate list n
 		(drop n list) ++ (take n list)
 
 
+rotate' list n
+	| (n > 0) = (drop n list) ++ (take n list) -- rotate left on +ve shift
+	| (n < 0) = (drop (len+n) list) ++ (take (len+n) list) -- rotate right on -ve shift
+	| otherwise = list -- n == 0
+	where len = length list
+
+
+{- using mod -}
+rotate'' list n = (drop len list) ++ (take len list)
+	where len = mod n (length list)
+
+
+{-
+ - Replicate list thrice
+ - "abcdefgh" -> "abcdefghabcdefghabcdefgh"
+ - rotate 3 -> drop (len + 3), -> "defghabcdefghabcdefgh"
+ -          -> take (len)  -> "defghabc"
+ -
+ -  rotate (-2) -> drop (len - 2) -> "ghabcdefghabcdefgh"
+ -				-> take (len) -> "ghabcdef"
+ -}
+rotate''' list n = take len $ drop (len+n) (list++list++list)
+	where len = length list
+
+
+-- alternately, use `cycle` which endlessly concatenates itself to `replicate`
+rotate'''' list n = take len $ drop (len+n) $ cycle list
+	where len = length list
+
 main = do
-	print $ rotate "abcdefgh" 3
-	print $ rotate "abcdefgh" (-2)
+	putStr $ assert ( (rotate "abcdefgh" 3) == "defghabc" ) "" 
+	putStr $ assert ( (rotate "abcdefgh" (-2)) == "ghabcdef" ) ""
+
+	putStr $ assert ( (rotate' "abcdefgh" 3) == "defghabc" ) "" 
+	putStr $ assert ( (rotate' "abcdefgh" (-2)) == "ghabcdef" ) ""
+
+	putStr $ assert ( (rotate'' "abcdefgh" 3) == "defghabc" ) "" 
+	putStr $ assert ( (rotate'' "abcdefgh" (-2)) == "ghabcdef" ) ""
+
+	putStr $ assert ( (rotate''' "abcdefgh" 3) == "defghabc" ) "" 
+	putStr $ assert ( (rotate''' "abcdefgh" (-2)) == "ghabcdef" ) ""
+
+	putStr $ assert ( (rotate'''' "abcdefgh" 3) == "defghabc" ) "" 
+	putStr $ assert ( (rotate'''' "abcdefgh" (-2)) == "ghabcdef" ) ""
 
