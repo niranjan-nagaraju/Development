7a407358f5199fd640ceb33311bd820629986a23
Niranjan Nagaraju <vinithepooh@gmail.com>
2021-10-02 16:06:59 +0530

    99 haskell problems, 20: removeAt - remove element at position k from a list

--- haskell/99_haskell_problems/20_removeAt.hs
+++ haskell/99_haskell_problems/20_removeAt.hs
@@ -1,10 +1,52 @@
-{- Remove the K'th element from a list. -}
+{-
+ - Problem 20
+ - (*) Remove the K'th element from a list.
+ -
+ - Example in Prolog:
+ -
+ - ?- remove_at(X,[a,b,c,d],2,R).
+ - X = b
+ - R = [a,c,d]
+ - Example in Lisp:
+ -
+ - * (remove-at '(a b c d) 2)
+ - (A C D)
+ - (Note that this only returns the residue list, while the Prolog version also returns the deleted element.)
+ -
+ - Example in Haskell:
+ -
+ - λ> removeAt 2 "abcd"
+ - ('b',"acd")
+ -}
 
+import Control.Exception( assert )
+
+{- using take, drop -}
 removeAt k list  = 
-	take (k-1) list ++ drop k list
+	(last firstK, (init firstK) ++ drop k list)
+	where
+		firstK = take k list
 
+
+{-
+ - separate list into left, right,
+ - when [left] has k elements,
+ -  skip last element from [left], and concat with the [right]
+ -  return (the skipped last element, rest of the list)
+ -}
+removeAt' k lst = removeAt'' k [] lst
+	where
+	removeAt'' k left right
+		| (length left == k) = (last left, init left ++ right)
+		| otherwise = removeAt'' k (left ++ [head right]) (tail right) 
+	
 main = do
-	print $ removeAt 1 "abcd"
-	print $ removeAt 2 "abcd"	
-	print $ removeAt 3 "abcd"	
-	print $ removeAt 4 "abcd"	
+	putStr $ assert ( (removeAt 1 "abcd") == ('a', "bcd") ) ""
+	putStr $ assert ( (removeAt 2 "abcd") == ('b', "acd") ) ""
+	putStr $ assert ( (removeAt 3 "abcd") == ('c', "abd") ) ""
+	putStr $ assert ( (removeAt 4 "abcd") == ('d', "abc") ) ""
+
+	putStr $ assert ( (removeAt' 1 "abcd") == ('a', "bcd") ) ""
+	putStr $ assert ( (removeAt' 2 "abcd") == ('b', "acd") ) ""
+	putStr $ assert ( (removeAt' 3 "abcd") == ('c', "abd") ) ""
+	putStr $ assert ( (removeAt' 4 "abcd") == ('d', "abc") ) ""
