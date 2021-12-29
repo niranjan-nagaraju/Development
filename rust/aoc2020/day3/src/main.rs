daa4742b1622daf1817dd1066f21b15770b64a7b
Niranjan Nagaraju <vinithepooh@gmail.com>
2021-12-29 20:55:46 +0530

    Aoc2020 - Day3, rust solution

--- rust/aoc2020/day3/src/main.rs
+++ rust/aoc2020/day3/src/main.rs
@@ -71,7 +71,7 @@ Determine the number of trees you would encounter if, for each of the following 
 
 In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s) respectively; multiplied together, these produce the answer 336.
 
-What do you get if you multiply together the number of trees encountered on each of the listed slopes?
+What do you get if you multiply together the number of trees encountered on each of the listed slopes? 6050183040
 */
 
 use std::io::{self, Read};
